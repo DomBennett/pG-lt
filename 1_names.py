731b3881fd805caf321f1d7e06b66c59b09b7c7f
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-03-24 21:59:41 +0000

    Created output, automated outgroup

--- 1_names.py
+++ 1_names.py
@@ -7,11 +7,10 @@
 print "\n\nStage 1: names resolution\n"
 
 ## Packages + Functions
-import os, re, sys, csv, pickle
+import os, re, sys, csv, pickle, shutil
 sys.path.append(os.path.join(os.getcwd(), 'functions'))
-from Bio import Phylo
-from taxon_names_resolver import TaxonNamesResolver
 from names_tools import *
+from taxon_names_resolver import TaxonNamesResolver
 
 ## Dirs
 info_dir = os.path.join(os.getcwd(), '0_info')
@@ -21,20 +20,39 @@ if not os.path.isdir(names_dir):
 	
 ## Input
 files = os.listdir(info_dir)
-input_file = sorted([e for e in files if re.search("^.*taxnames.txt$", e)])
+taxnames_file = sorted([e for e in files if re.search("^.*taxnames.txt$", e)])[0]
+parentid_file = sorted([e for e in files if re.search("^.*parentID.txt$", e)])[0]
 current_study = re.sub("_taxnames.txt$", "", taxnames_file)
-print '\n\nWorking on: [{0}]'.format(current_study)
 
 ## Process
-print 'Searching for taxids ...'
-taxon_id = int(taxadict[current_study][0])
-input_file = os.path.join(input_dir, taxnames_file)
-resolver = TaxonNamesResolver(input_file = input_file, datasource = "NCBI", taxon_id = taxon_id)
+print 'Searching for taxids'
+with open(os.path.join(info_dir, parentid_file), "r") as file:
+	parent_id = int(file.read())
+input_file = os.path.join(info_dir, taxnames_file)
+resolver = TaxonNamesResolver(input_file = input_file, datasource = "NCBI", taxon_id = parent_id)
 resolver.main()
+print "Generating names dictionary"
 namesdict = genNamesDict(resolver)
-print '... generating taxonomic tree ...'
+print 'Generating taxonomic tree'
 taxontree,shared_lineages = genTaxTree(resolver, namesdict, draw = True)
 
 ## Output
-
+pickle.dump(namesdict, open(os.path.join(info_dir, "namesdict.p"), "wb"))
+shutil.rmtree("resolved_names")
+headers = ["MPE_id", "name", "unique_name", "rank", "Genbank_ids"]
+with open(os.path.join(names_dir, 'resovled_names.csv'), 'wb') as file:
+	writer = csv.writer(file)
+	writer.writerow(headers)
+	for key in namesdict.keys():
+		temp = namesdict[key]
+		row = [key, temp["name"], temp["unique_name"], temp["rank"]]
+		if len(temp["ids"]) > 1:
+			ids = ""
+			for each in temp["ids"]:
+				ids += str(each) + "|"
+		else:
+			ids = temp["ids"][0]
+		row.append(ids)
+		writer.writerow(row)
+Phylo.write(taxontree, os.path.join(info_dir, "taxontree.tre"), "newick")
 print 'Stage finished. Resolved [{0}] names.'.format(len(namesdict.keys()))
