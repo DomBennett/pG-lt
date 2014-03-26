bf1f4d6feb379ca9512a1d708fb230be33fbc5b7
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-03-26 01:27:39 +0000

    New phylogeny script and tools

--- 4_phylogeny.py
+++ 4_phylogeny.py
@@ -3,112 +3,78 @@
 ## D.J. Bennet
 ## 24/03/2014
 
-## Parameters
-niterations = 100
-max_branch = 0.5
-max_trys = 1000
-
 ## Print stage
 print "\n\nThis is stage 4: phylogenies\n"
 
 ## Packages
-import sys, os, re, random
+import os, re, random, pickle
 from Bio import AlignIO
 from Bio import Phylo
 import dendropy as dp
-import numpy as np
-import phylogeny_tools
+from phylogeny_tools import *
 
-## dirs
-names_dir = os.path.join(os.getcwd(), '1_taxids')
-names_files = os.listdir(names_dir)
-input_dir = os.path.join(os.getcwd(), '3_alignments')
-output_dir = os.path.join(os.getcwd(), '4_phylogenies')
-if not os.path.isdir(output_dir):
-	os.mkdir(output_dir)
+## Dirs
+alignment_dir = os.path.join(os.getcwd(),'3_alignment')
+phylogeny_dir = os.path.join(os.getcwd(),'4_phylgeny')
+if not os.path.isdir(phylogeny_dir):
+	os.mkdir(phylogeny_dir)
 
-## Loop through studies
-studies = sorted(os.listdir(input_dir))
-studies = [st for st in studies if not re.search("^\.|^log\.txt$", st)]
-study_counter = 0
-print 'Looping through studies ...'
-nphylos_all = 0
-for i in range(2,len(studies)):
-	print '\n\nWorking on: [{0}]'.format(studies[i])
-	taxontree_file = os.path.join(names_dir, "{0}_taxontree.txt".\
-		format(studies[i]))
-	study_names_files = [e for e in names_files if\
-		re.search("^{0}".format(studies[i]), e)]
-	taxids_file = [e for e in study_names_files if\
-		re.search("taxids", e)][0]
-	qnames_file = [e for e in study_names_files if\
-		re.search("qnames", e)][0]
-	names = {}
-	with file(os.path.join(names_dir, taxids_file), 'rb') as taxfile:
-		with file(os.path.join(names_dir, qnames_file), 'rb')\
-			as qfile:
-			for txid,qname in zip(taxfile, qfile):        
-				txid = "tx" + txid.strip()
-				# spaces need to be repalced with _
-				names[txid] = re.sub("\\s", "_", qname.strip())
-	study_dir = os.path.join(input_dir, studies[i])
-	genes = sorted(os.listdir(study_dir))
-	genes = [e for e in genes if not re.search("^\.", e)]
-	print "Reading in alignments ..."
-	align_obj = {}
-	for gene in genes:
-		align_obj[gene] = []
-		gene_dir = os.path.join(study_dir, gene)
-		a_files = os.listdir(gene_dir)
-		a_files = [e for e in a_files if not re.search("^\.", e)]
-		for a_file in a_files:
-			a_file_path = os.path.join(gene_dir, a_file)
-			align = AlignIO.read(a_file_path, "fasta")
-			align_obj[gene].append(align)
-	naligns = [len(align_obj[e]) for e in align_obj.keys()]
-	print "Done. Read in [{0}] alignments for [{1}]".format(naligns,align_obj.keys())
-	## run phylogenies
-	print "Generating [{0}] phylogenies ...".format(niterations)
-	nphylos = 0
-	counter = 0
-	phylos = []
-	while nphylos < niterations:
-		if counter > max_trys:
-			break
-		counter += 1
-		print counter
-		alignments = [random.sample(align_obj[e], 1)[0] for e in genes]
-		print taxontree_file
-		alignment,partitions = concatenateAlignments(alignments)
-		#constraint = genConstraintTree(alignment, taxontree_file)
-		if "darwin" == sys.platform:
-			phylo = RAxML(alignment, method = 'localVersion', constraint = False, outgroup = "outgroup", partitions = partitions)
-		else:
-			phylo = RAxML(alignment, method = "raxmlHPC")
-		Phylo.draw_ascii(phylo)
-		try:
-			phylo.root_with_outgroup("outgroup")
-		except ValueError:
-			continue
-		phylo.prune("outgroup")
-		phylo = renameTips(phylo, names)
-		Phylo.draw_ascii(phylo)
-		if phyloTest(phylo, taxontree_file, max_nsplits, max_branch):
-			print "A good tree!"
-			phylos.append(phylo)
-			nphylos += 1
-		else:
-			print "A bad tree..."
-	if len(phylos) > 1:
-		print 'Done. [{0}] phylogenies for [{1}].'.format(nphylos, studies[i])
-		print 'Writing out phylogenies and generating consensus.'
-		filepath = os.path.join(output_dir, studies[i] + "_phylos" + '.tre')
-		Phylo.write(phylos, filepath, 'newick')
-		phylos = dp.TreeList()
-		phylos.read(open(filepath, "rU"), "newick")
-		consensus = phylos.consensus(min_freq = 0.5)
-		consensus.write_to_path(os.path.join(output_dir, studies[i] + "_consensus" + '.tre'),\
-									"newick", suppress_edge_lengths = True)
-		study_counter += 1
-		nphylos_all += nphylos
-print '\n\nStage finished. Generated [{0}] phylogenies across [{1}] studies.'.format(nphylos_all, study_counter)
+## Input
+with open("genedict.p", "rb") as file:
+	genedict = pickle.load(file)
+with open("paradict.p", "rb") as file:
+	paradict = pickle.load(file)
+with open("namesdict.p", "rb") as file:
+	namesdict = pickle.load(file)
+
+## Parameters
+#nphylos = int(paradict["nphylos"])
+nphylos = 10
+maxtrys = int(paradict["maxtrys"])
+maxpedge = float(paradict["maxpedge"])
+phylocounter = 0
+
+## Process
+genes = sorted(os.listdir(alignment_dir))
+genes = [e for e in genes if not re.search("^\.|^log\.txt$", e)]
+print "Reading in alignments"
+alignobj = {}
+for gene in genes:
+	alignobj[gene] = []
+	gene_dir = os.path.join(alignment_dir, gene)
+	alignment_files = os.listdir(gene_dir)
+	alignment_files = [e for e in alignment_files if not re.search("^\.", e)]
+	for alignment_file in alignment_files:
+		with open(os.path.join(gene_dir, alignment_file), "r") as file:
+			alignment = AlignIO.read(file, "fasta")
+		alignobj[gene].append(alignment)
+print "Generating [{0}] phylogenies".format(nphylos)
+phylogenies = []
+trys = 0
+while phylocounter < nphylos:
+	print "Iteration [{0}]".format(phylocounter)
+	if trys > maxtrys:
+		break
+	trys += 1
+	alignments = [random.sample(alignobj[e], 1)[0] for e in genes]
+	alignment,partitions = concatenateAlignments(alignments)
+	constraint = genConstraintTree(alignment, "taxontree.tre")
+	phylogeny = RAxML(alignment, constraint = constraint, outgroup = "outgroup",\
+		partitions = partitions)
+	phylogeny.root_with_outgroup("outgroup")
+	phylogeny.prune("outgroup")
+	phylogeny = renameTips(phylogeny, namesdict)
+	#Phylo.draw_ascii(phylogeny)
+	if goodPhylogenyTest(phylogeny, maxpedge):
+		phylogenies.append(phylogeny)
+		phylocounter += 1
+print 'Generating consensus.'
+filepath = os.path.join(phylogeny_dir, 'distribution.tre')
+with open(filepath, "w") as file:
+	Phylo.write(phylogenies, file, 'newick')
+phylos = dp.TreeList()
+phylos.read(open(filepath, "rU"), "newick")
+consensus = phylos.consensus(min_freq = 0.5)
+consensus.write_to_path(os.path.join(phylogeny_dir, "consensus.tre"), "newick",\
+	suppress_edge_lengths = True)
+print '\n\nStage finished. Generated [{0}] phylogenies.'.format(phylocounter)
---
+++ phylogeny_tools.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+## MPE Phylogeny tools
+## D.J. Bennet
+## 24/03/2014
+
+## Packages
+import sys, os, re, random
+import dendropy as dp
+from Bio import Phylo
+from Bio.Seq import Seq
+from Bio.Align import MultipleSeqAlignment
+from Bio.SeqRecord import SeqRecord
+from Bio import Phylo
+from Bio import AlignIO
+import numpy as np
+from sys_tools import *
+
+## Functions
+def renameTips(phylo, names):
+	for each in phylo.get_terminals():
+		try:
+			each.name = names[each.name]["name"]
+		except KeyError:
+			pass
+	return phylo
+
+def getRTTDists(phylo):
+	"""Calcualte root to tips distance"""
+	names = []
+	for terminal in phylo.get_terminals():
+		names.append(terminal.name)
+	rtt_dists = []
+	for name in names:
+		rtt_dists.append(phylo.distance(name))
+	return rtt_dists
+
+def getTBP(phylo):
+	term_lens = []
+	for terminal in phylo.get_terminals():
+		term_lens.append(terminal.branch_length)
+	total_len = phylo.total_branch_length()
+	tbps = []
+	for term_len in term_lens:
+		tbps.append(term_len/total_len)
+	return tbps
+
+def getBranchLengths(phylo):
+	lens = []
+	depths =  phylo.depths(unit_branch_lengths = True)
+	for branch in depths.keys():
+		if branch.branch_length:
+			lens.append(branch.branch_length)
+	return lens
+	
+def goodPhylogenyTest(query, max_branch):
+	lens = getBranchLengths(query)
+	total_len = sum(lens)
+	lens_bool = [e/total_len > max_branch for e in lens]
+	if any(lens_bool):
+		return False
+	else:
+		return True
+
+# def phyloTest(query, ref_path, max_nsplits, max_branch):
+# 	lens = getBranchLengths(query)
+# 	total_len = sum(lens)
+# 	lens_bool = [e/total_len > max_branch for e in lens]
+# 	if any(lens_bool):
+# 		return False
+# 	elif max_nsplits:
+# 		tax_tree = dp.Tree()
+# 		tax_tree.read_from_path(ref_path, "newick")
+# 		temp_tree = dp.Tree()
+# 		Phylo.write(query, "temp_phylo.txt", 'newick')
+# 		temp_tree.read_from_path("temp_phylo.txt", "newick")
+# 		os.remove("temp_phylo.txt")
+# 		# I don't think it's necessary for them to have the same number of taxa
+# 		tax_list = [e for e in tax_tree.taxon_set]
+# 		temp_list = [e for e in temp_tree.taxon_set]
+# 		drop_list = [e1 for e1 in tax_list if e1.label not in [e2.label for e2 in temp_list]]
+# 		tax_tree.prune_taxa(drop_list)
+# 		print tax_tree.as_ascii_plot()
+# 		# extract number of splits in the tax_tree not in temp_tree
+# 		# http://pythonhosted.org/DendroPy/tutorial/treestats.html#frequency-of-a-split-in-a-collection-of-trees
+# 		nsplits = tax_tree.false_positives_and_negatives(temp_tree)
+# 		print "... [{0}] tax splits, [{1}] temp splits...".format(nsplits[0], nsplits[1])
+# 		if nsplits[1] > max_nsplits:
+# 			return False
+# 		else:
+# 			return True
+# 	else:
+# 		return True
+
+def genConstraintTree(alignment, taxontree_file):
+	tip_names = []
+	for record in alignment:
+		tip_names.append(record.id)
+	with open(taxontree_file, "r") as file:
+		constraint = Phylo.read(file, "newick")
+	constraint_tips = []
+	for terminal in constraint.get_terminals():
+		constraint_tips.append(terminal.name)
+	tips_to_drop = [e for e in constraint_tips if not e in tip_names]
+	for tip in tips_to_drop:
+		constraint.prune(tip)
+	with open("constraint.tre", "w") as file:
+		Phylo.write(constraint, file, "newick")
+	if constraint.is_bifurcating():
+		return " -r constraint.tre"
+	else:
+		return " -g constraint.tre"
+
+def concatenateAlignments(alignments):
+	if len(alignments) == 1:
+		return alignments,False
+	# Sort IDs
+	alignment_ids = []
+	for gene in alignments:
+		gene_ids = []
+		for rec in gene:
+			gene_ids.append(rec.id)
+		alignment_ids.append(gene_ids)
+	all_ids = []
+	[all_ids.extend(e) for e in alignment_ids]
+	all_ids = list(set(all_ids))
+	# Concatenate
+	alignment = MultipleSeqAlignment([])
+	for txid in all_ids:
+		sequence = ""
+		for i,gene in enumerate(alignments):
+			if txid in alignment_ids[i]:
+				sequence += gene[alignment_ids[i].index(txid)].seq
+			else:
+				sequence += "-" * gene.get_alignment_length()
+		sequence = SeqRecord(sequence, id = txid, description = "multigene sequence")
+		alignment.append(sequence)
+	# Get partitions
+	lengths = [e.get_alignment_length() for e in alignments]
+	partitions = [0].append(np.cumsum(lengths))
+	return alignment,partitions
+
+def RAxML(alignment, outgroup=None, partitions=None, constraint=None, timeout=999999999):
+	"""Adapted pG function: Generate phylogeny from alignment using RAxML (external program)."""
+	input_file = 'phylogeny_in.phylip'
+	output_file = 'phylogeny_out'
+	file_line = ' -s ' + input_file + ' -n ' + output_file
+	options = ' -p ' + str(random.randint(0,10000000))
+	if outgroup:
+		options += ' -o ' + outgroup
+	with open(input_file, "w") as file:
+		AlignIO.write(alignment, file, "phylip-relaxed")
+	if len(alignment) > 100:
+		dnamodel = ' -m GTRCAT'
+	else:
+		dnamodel = ' -m GTRGAMMA'
+	if partitions:
+		with open("partitions.txt", 'w') as f:
+			for i in range(0, len(partitions)-1):
+				f.write("DNA, position" + str(partitions[i]) + " = " + str(partitions[i]+1) +\
+					"-" + str(partitions[i+1]) + "\n")
+		options += " -q " + "partitions.txt"
+	if constraint:
+		options += constraint
+	command_line = 'raxml' + file_line + dnamodel + options
+	pipe = TerminationPipe(command_line, timeout)
+	pipe.run(changeDir = True)
+	if not pipe.failure:
+		with open('RAxML_bestTree.' + output_file, "r") as file:
+			tree = Phylo.read(file, "newick")	
+		if constraint:
+			os.remove('constraint.tre')
+		if partitions:
+			os.remove("partitions.txt")
+		os.remove(input_file)
+		all_files = os.listdir(os.getcwd())
+		for each in all_files:
+			if re.search("(RAxML)", each):
+				os.remove(each)
+			if "In.phylip.reduced" == each:
+				os.remove(each)
+		return tree
+	else:
+		raise RuntimeError("Either phylogeny building program failed, or ran out of time")
+
+if __name__ == '__main__':
+	pass
