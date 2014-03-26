e183910e7583a739d9f77b29eae6230ba7466081
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-03-26 01:27:10 +0000

    New names script and tools

--- 1_names.py
+++ 1_names.py
@@ -6,40 +6,47 @@
 ## Print stage
 print "\n\nStage 1: names resolution\n"
 
-## Packages + Functions
+## Packages
 import os, re, sys, csv, pickle, shutil
-sys.path.append(os.path.join(os.getcwd(), 'functions'))
 from names_tools import *
 from taxon_names_resolver import TaxonNamesResolver
 
 ## Dirs
-info_dir = os.path.join(os.getcwd(), '0_info')
-names_dir = os.path.join(os.getcwd(), '1_names')
+names_dir = os.path.join(os.getcwd(),'1_names')
 if not os.path.isdir(names_dir):
 	os.mkdir(names_dir)
-	
+
 ## Input
-files = os.listdir(info_dir)
-taxnames_file = sorted([e for e in files if re.search("^.*taxnames.txt$", e)])[0]
-parentid_file = sorted([e for e in files if re.search("^.*parentID.txt$", e)])[0]
-current_study = re.sub("_taxnames.txt$", "", taxnames_file)
+with open("genedict.p", "rb") as file:
+	genedict = pickle.load(file)
+with open("paradict.p", "rb") as file:
+	paradict = pickle.load(file)
+with open("terms.p", "rb") as file:
+	terms = pickle.load(file)
+
+## Parameters
+Entrez.email = paradict["email"]
 
 ## Process
 print 'Searching for taxids'
-with open(os.path.join(info_dir, parentid_file), "r") as file:
-	parent_id = int(file.read())
-input_file = os.path.join(info_dir, taxnames_file)
-resolver = TaxonNamesResolver(input_file = input_file, datasource = "NCBI", taxon_id = parent_id)
+try:
+	parentid = int(paradict["parentid"])
+except:
+	parentid = False
+resolver = TaxonNamesResolver(terms = terms, datasource = "NCBI", taxon_id = parentid)
 resolver.main()
 print "Generating names dictionary"
-namesdict = genNamesDict(resolver)
+namesdict,all_ids = genNamesDict(resolver)
 print 'Generating taxonomic tree'
-taxontree,shared_lineages = genTaxTree(resolver, namesdict, draw = True)
+taxontree,shared_lineages = genTaxTree(resolver, namesdict)
 
 ## Output
-pickle.dump(namesdict, open(os.path.join(info_dir, "namesdict.p"), "wb"))
 shutil.rmtree("resolved_names")
-headers = ["MPE_id", "name", "unique_name", "rank", "Genbank_ids"]
+with open("namesdict.p", "wb") as file:
+	pickle.dump(namesdict, file)
+with open("all_ids.p", "wb") as file:
+	pickle.dump(all_ids, file)
+headers = ["MPE_id", "name", "unique_name", "rank", "NCBI_Taxids"]
 with open(os.path.join(names_dir, 'resovled_names.csv'), 'wb') as file:
 	writer = csv.writer(file)
 	writer.writerow(headers)
@@ -54,5 +61,7 @@ with open(os.path.join(names_dir, 'resovled_names.csv'), 'wb') as file:
 			ids = temp["ids"][0]
 		row.append(ids)
 		writer.writerow(row)
-Phylo.write(taxontree, os.path.join(info_dir, "taxontree.tre"), "newick")
-print 'Stage finished. Resolved [{0}] names.'.format(len(namesdict.keys()))
+Phylo.write(taxontree, "taxontree.tre", "newick")
+
+## Print stageout
+print 'Stage finished. Resolved [{0}] names including outgroup.'.format(len(namesdict.keys()))
