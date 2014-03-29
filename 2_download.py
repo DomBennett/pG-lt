c0ec8dce61bd8987725b981c4c3bfbbd8ef3b809
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-03-29 15:52:34 +0000

    Redeveloped download
    Created a download object to make downloading more efficient

--- 2_download.py
+++ 2_download.py
@@ -23,7 +23,7 @@ with open("paradict.p", "rb") as file:
 	paradict = pickle.load(file)
 with open("namesdict.p", "rb") as file:
 	namesdict = pickle.load(file)
-with open("all_ids.p", "rb") as file:
+with open("allrankids.p", "rb") as file:
 	allrankids = pickle.load(file)
 
 ## Parameters
@@ -32,18 +32,19 @@ nseqs = int(paradict['nseqs'])
 thoroughness = int(paradict['download_thoroughness'])
 maxlen = int(paradict['maxlen'])
 minlen = int(paradict['minlen'])
-filter_threshold = int(paradict['filter_threshold'])
-filter_seed = 5
+seedsize = 5
 mingaps = 0.01
 minoverlap = 200
 maxtrys = 100
 minnseq = 1
 minpwithseq = 0.6
+maxpn = 0.1
 seqcounter = basecounter = spcounter = 0
 
 ## Process
 print 'Determining best genes'
-genes = findBestGenes(namesdict, genedict, thoroughness, allrankids, minnseq, minpwithseq)
+#genes = findBestGenes(namesdict, genedict, thoroughness, allrankids, minnseq, minpwithseq)
+genes = ["matk", "rbcl"]
 statement = 'Using genes:'
 for gene in genes:
 	statement += " " + gene
@@ -55,45 +56,20 @@ for gene in genes:
 	gene_dir  = os.path.join(download_dir, str(gene))
 	if not os.path.isdir(gene_dir):
 		os.mkdir(gene_dir)
-	for nameid in namesdict.keys():
-		print "..... [{0}]: [{1}]".format(nameid,namesdict[nameid]["name"])
-		taxids = namesdict[nameid]["ids"]
-		sequences = []
-		seqids = sequenceSearch(taxids, gene_names, thoroughness)
-		if len(seqids) >= nseqs: # Only filter if more than 100 in genbank
-			print "Lots of sequences for [{0}]. Downloading and filtering.".format(nameid)
-			for taxid in taxids:
-				temp_taxids = findChildren(taxid)
-				downloaded = []
-				deja_vues = []
-				while len(sequences) < nseqs:
-					downloaded,temp_deja_vues = sequenceDownload(temp_taxids, gene_names, deja_vues, minlen, maxlen,\
-						maxpn = 0.01, thoroughness = thoroughness, nseqs = nseqs, seq_ids = seqids)
-					if len(downloaded) == 0: # keeps looping until no more new sequences are being downloaded
-						break
-					deja_vues.extend(temp_deja_vues)
-					deja_vues = list(set(deja_vues))
-					if len(downloaded) < filter_seed and len(temp_taxids) < 1: # only skip the filtering stage if few temp_taxids
-						sequences.extend(downloaded)
-						break
-					else:
-						filtered,downloaded = filterSequences(sequences = downloaded, filter_seed = filter_seed,\
-							mingaps = mingaps, minoverlap = minoverlap, maxtrys = maxtrys, minlen = minlen)
-					if len(filtered) > 0:
-						sequences.extend(filtered)
-		else:
-			sequences,_ = sequenceDownload(taxids, gene_names, [], minlen, maxlen, maxpn = 0.01,\
-				thoroughness = thoroughness, nseqs = nseqs, seq_ids = seqids)
-		if len(sequences) < 1:
+	for name in namesdict.keys():
+		print "..... [{0}]".format(name)
+		taxids = namesdict[name]["txids"]
+		sequence_downloader = SequenceDownloader(gene_names, nseqs, thoroughness,\
+			maxpn, seedsize, maxtrys, mingaps, minoverlap, maxlen, minlen)
+		sequences = sequence_downloader.main(taxids)
+		if not sequences:
 			noseqcounter_gene += 1
-			print "No sequences found for [id{0}].".format(nameid)
+			print "No sequences found for [{0}].".format(name)
 			continue
-		if len(sequences) > nseqs:
-			sequences = random.sample(sequences, nseqs)
 		gene_seqs = []
 		for seq in sequences:
 			gene_seqs.append(seq.format('fasta'))
-		with open(os.path.join(gene_dir, str(nameid) + '.fasta'), 'wb') \
+		with open(os.path.join(gene_dir, "{0}.fasta".format(name)), 'wb') \
 		as outfile:
 			for gene_seq in gene_seqs:
 				outfile.write("%s\n" % gene_seq)
--- download_tools.py
+++ download_tools.py
@@ -9,109 +9,42 @@ from entrez_tools import *
 from alignment_tools import *
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 
-## Functions
-def filterSequences(sequences, filter_seed, maxtrys, mingaps, minoverlap, minlen):
-	if len(sequences) < filter_seed:
-		return [], sequences
-	def filterByAlignment(sequences):
-		align = alignSequences(sequences)
-		return alignCheck(align, mingaps, minoverlap, minlen)
-	filtered = []
-	trys = 0
-	while filter_seed > 2 and trys < maxtrys:
-		temp = []
-		for i in range(filter_seed):
-			randn = random.randint(0, len(sequences)-1)
-			temp.append(sequences.pop(randn))
-		if filterByAlignment(temp):
-			filtered.extend(temp)
-		else:
-			sequences.extend(temp)
-			trys += 1
-		if len(sequences) < filter_seed*2:
-			filter_seed -= 1
-		if len(sequences) < filter_seed:
-			filter_seed = len(sequences) - 1
-	return filtered,sequences
+## Objects
+class SequenceDownloader(object):
+	"""Download sequences given taxids and gene_names"""
+	def __init__(self, gene_names, nseqs, thoroughness, maxpn, seedsize, maxtrys, \
+			mingaps, minoverlap, maxlen, minlen):
+		self.gene_names = gene_names
+		self.nseqs = nseqs
+		self.max_thoroughness = thoroughness
+		self.maxpn = maxpn
+		self.seedsize = seedsize
+		self.maxtrys = maxtrys
+		self.mingaps = mingaps
+		self.minoverlap = minoverlap
+		self.maxlen = maxlen
+		self.minlen = minlen
+		self.temp_taxids = None
+		self.seqs_deja_vues = []
+		self.taxids_deja_vues = []
+		self.pattern = re.compile("[ACTGactg]")
 
-def findGeneInSeq(record, gene_names):
-	"""Adapted pG function. Extract gene sequence from larger sequence by searching sequence features.
-
-	Arguments:
-	 record = SeqRecord object
-	 gene_names = list of synonyms for gene of interest
-
-	Return:
-	 SeqRecord object or False (if feature not found)"""
-	try:
-		if record.features:
-			for feature in record.features:
-				feature_names = []
-				if 'gene' in feature.qualifiers.keys():
-					feature_names.extend(feature.qualifiers['gene'])
-				if 'gene_synonym' in feature.qualifiers.keys():
-					feature_names.extend(feature.qualifiers['gene_synonym'])
-				if 'product' in feature.qualifiers.keys():
-					feature_names.extend(feature.qualifiers['product'])
-				gene_names = [e.lower() for e in gene_names]
-				feature_names = [e.lower() for e in feature_names]
-				if set(gene_names) & set(feature_names):
-					extractor = SeqFeature(feature.location)
-					found_seq = extractor.extract(record)
-					return found_seq # dropped TrimSeq (as I didn't know what it did...)
-			else:
-				print "... findGeneInSeq: can't find gene [{0}] in sequence [{1}]".\
-					format(gene_names[0], record.id)
-				return ()
-		else:
-			print "... findGeneInSeq: can't find features in in sequence [{1}]".\
-				format(record.id)
-			return ()
-	except ValueError: # catch value errors raised for sequences with "fuzzy" positions
-		return ()
-
-def findBestGenes(namesdict, genedict, thoroughness, allrankids, minnseq = 1, minpwithseq = 0.5):
-	alltipids = [namesdict[e]["ids"] for e in namesdict.keys()]
-	genes = []
-	for gene in genedict.keys():
-		print "Checking [{0}]".format(gene)
-		taxid = genedict[gene]["taxid"]
-		if int(taxid) in allrankids:
-			gene_bool = []
-			for tipids in alltipids:
-				res = sequenceSearch(tipids, genedict[gene]["names"], thoroughness)
-				gene_bool.append(len(res) >= minnseq)
-			pwithseq = float(sum(gene_bool))/len(alltipids)
-			if pwithseq > minpwithseq:
-				genes.append(gene)
-	return genes
-
-def sequenceSearch(taxids, gene_names, thoroughness, deja_vues = []):
-	"""Search for sequences given a taxid and gene names.
-
-	Arguments:
-	 taxids = taxon ids (list)
-	 gene_names = list of synonyms for gene of interest
-	 thoroughness = a number between 1 and 3 determining how thorough
-	  a search should be
-
-	 Return:
-	  List of Sequence IDs"""
-	def buildSearchTerm(taxids, gene_names, phase):
+	def _buildSearchTerm(self, taxids, thoroughness):
+		"""Generate NCBI GenBank query given taxids, gene_names and thoroughness"""
 		taxids_term = "txid{0}[PORGN]".format(taxids[0])
 		for taxid in taxids[1:]:
 			taxids_term = ("({0}) OR txid{1}[PORGN]".\
 						 format(taxids_term, taxid))
-		# for field see: http://www.ncbi.nlm.nih.gov/books/NBK49540/
-		gene_names = ["\"{0}\"".format(e) for e in gene_names]
-		if phase == 1: # use gene field and ignore: predicted, genome
+		# for fields see: http://www.ncbi.nlm.nih.gov/books/NBK49540/
+		gene_names = ["\"{0}\"".format(e) for e in self.gene_names]
+		if thoroughness == 1: # use gene field and ignore: predicted, genome
 			gene_term = ("{0}[GENE]".format(gene_names[0]))
 			for gene_name in gene_names[1:]:
 				gene_term = ("({0}) OR {1}[GENE]".\
 							 format(gene_term, gene_name))
 			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT genome[TI] NOT unverified[TI]".\
 						   format(taxids_term, gene_term))
-		elif phase == 2: # use title for gene name and ignore: predicted, genome
+		elif thoroughness == 2: # use title for gene name and ignore: predicted, genome
 			gene_term = ("{0}[TI]".format(gene_names[0]))
 			for gene_name in gene_names[1:]:
 				gene_term = ("({0}) OR {1}[TI]".\
@@ -125,39 +58,78 @@ def sequenceSearch(taxids, gene_names, thoroughness, deja_vues = []):
 							 format(gene_term, gene_name))
 			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT shotgun[TI] NOT scaffold[TI] NOT assembly[TI] NOT unverified[TI]".\
 						   format(taxids_term, gene_term))
+		print search_term
 		return search_term
-	seq_ids = []
-	count = 0
-	phase = 1
-	while len(seq_ids) == 0:
-		if phase > thoroughness:
-			return []
-		search_term = buildSearchTerm(taxids, gene_names, phase)
-		seqcount = eSearch(search_term)['Count']
-		if int(seqcount) >= 1:
-			seq_ids.extend(eSearch(search_term, retMax = seqcount)['IdList'])
-			seq_ids = [e for e in seq_ids if not e in deja_vues]
-		phase += 1
-	return list(set(seq_ids))
 
-def sequenceDownload(taxids, gene_names, deja_vues, minlen, maxlen,\
-				 maxpn, thoroughness, nseqs, seq_ids = None):
-	"""Download and parse sequences given a taxid and gene names or list of sequence IDs.
+	def _search(self, taxids):
+		"""Search GenBank for matches, increasing thoroughness if no matches"""
+		seqids = []
+		self.thoroughness = 1
+		while len(seqids) == 0:
+			if self.thoroughness > self.max_thoroughness:
+				return None
+			search_term = self._buildSearchTerm(taxids, self.thoroughness)
+			seqcount = eSearch(search_term)['Count']
+			if int(seqcount) >= 1:
+				seqids.extend(eSearch(search_term, retMax = seqcount)['IdList'])
+				seqids = [e for e in seqids if not e in self.seqs_deja_vues]
+			self.thoroughness += 1
+		self._DejaVue(seqids)
+		return list(set(seqids))
 
-	Arguments:
-	 taxids = taxon ids (list)
-	 gene_names = list of synonyms for gene of interest
-	 minlen = min sequence length
-	 maxlen = max sequence length
-	 maxpn = max proportion of ambiguous nucleotides
-	 thoroughness = a number between 1 and 3 determining how thorough
-	  a search should be
-	 nseqs = max number of sequences to download (default 100)
-	 seq_ids = list of pre-specified sequence IDs
+	def _filterSequences(self, sequences):
+		"""Filter sequences by aligning sequences from closely organisms"""
+		if len(sequences) < self.seedsize:
+			return sequences
+		def filterByAlignment(sequences):
+			alignment = alignSequences(sequences)
+			return alignmentCheck(alignment, self.mingaps, self.minoverlap, self.minlen)
+		filtered = []
+		trys = 0
+		seedsize = self.seedsize
+		while seedsize > 2 and trys < self.maxtrys:
+			temp = []
+			for i in range(seedsize):
+				randn = random.randint(0, len(sequences)-1)
+				temp.append(sequences.pop(randn))
+			if filterByAlignment(temp):
+				filtered.extend(temp)
+			else:
+				sequences.extend(temp)
+				trys += 1
+			if len(sequences) < seedsize*2:
+				seedsize -= 1
+			if len(sequences) < seedsize:
+				seedsize = len(sequences) - 1
+		return filtered
 
-	Return:
-	 List of SeqRecord objects"""
-	def parse(record):
+	def _findGeneInSeq(self, record):
+		"""Extract gene sequence from larger sequence by searching features."""
+		try:
+			if record.features:
+				for feature in record.features:
+					feature_names = []
+					if 'gene' in feature.qualifiers.keys():
+						feature_names.extend(feature.qualifiers['gene'])
+					if 'gene_synonym' in feature.qualifiers.keys():
+						feature_names.extend(feature.qualifiers['gene_synonym'])
+					if 'product' in feature.qualifiers.keys():
+						feature_names.extend(feature.qualifiers['product'])
+					gene_names = [e.lower() for e in self.gene_names]
+					feature_names = [e.lower() for e in feature_names]
+					if set(gene_names) & set(feature_names):
+						extractor = SeqFeature(feature.location)
+						found_seq = extractor.extract(record)
+						return found_seq
+				else:
+					return None
+			else:
+				return None
+		except ValueError: # catch value errors raised for sequences with "fuzzy" positions
+			return None
+
+	def _parse(self, record):
+		"""Parse record returned from GenBank"""
 		if isinstance(record, list):
 			# find which sequence in the list has the gene
 			for each in record:
@@ -166,41 +138,114 @@ def sequenceDownload(taxids, gene_names, deja_vues, minlen, maxlen,\
 						record = each
 						break
 			else:
-				return False
-		if len(record) > maxlen:
-			record = findGeneInSeq(record, gene_names)
-		if maxlen > len(record) > minlen:
+				return None
+		if len(record) > self.maxlen:
+			record = self._findGeneInSeq(record)
+		if self.maxlen > len(record) > self.minlen:
 			# find proportion of ambiguous bases
-			nn = len(pattern.findall(record.seq.tostring()))
+			nn = len(self.pattern.findall(record.seq.tostring()))
 			pn = float(len(record)) - float(nn)
 			pn = pn/float(len(record))
-			if pn < maxpn:
+			if pn < self.maxpn:
 				return record
-		return False
-	if seq_ids is None:
-		seq_store = sequenceSearch(taxids, gene_names, thoroughness, deja_vues)
-		deja_vues = seq_store[:]
-	else:
-		seq_store = seq_ids[:]
-	pattern = re.compile("[ACTGactg]")
-	records = []
-	i = 1
-	while i <= nseqs and len(seq_store) > 0:
-		if len(seq_store) > 100:
-			n = 100 # Download in chunks of 100
+		return None
+
+	def _download(self, seqids):
+		"""Download records from genbank given sequence ids"""
+		records = []
+		i = 1
+		while i <= self.nseqs and len(seqids) > 0:
+			if len(seqids) > 100:
+				n = 100 # Download in chunks of 100
+			else:
+				n = len(seqids)
+			seqs = []
+			for _ in range(n):
+				randi = random.randint(0, len(seqids)-1)
+				seqs.append(seqids.pop(randi))
+			for record in eFetch(seqs):
+				record = self._parse(record)
+				if record:
+					records.append(record)
+					i += 1
+		return records
+
+	def _findGenera(self, taxids):
+		"""Randomly return a taxid of decendant genera (or below). None if genera deja-vue."""
+		if self.temp_taxids:
+			temp_taxid = self.temp_taxids.pop(-1)
 		else:
-			n = len(seq_store)
-		seqs = []
-		for _ in range(n):
-			randi = random.randint(0, len(seq_store)-1)
-			seqs.append(seq_store.pop(randi))
-		for record in eFetch(seqs):
-			print record.id
-			record = parse(record)
-			if record:
-				records.append(record)
-				i += 1
-	return records,deja_vues
+			taxid = random.sample(taxids, 1)[0]
+			temp_taxids = findChildren(taxid, target = 1)
+			temp_taxid = random.sample(temp_taxids, 1)[0]
+		if temp_taxid in self.taxids_deja_vues:
+			return None
+		else:
+			self._DejaVue(temp_taxid, sequences = False)
+			return [temp_taxid]
+
+	def _DejaVue(self, vues, sequences = True):
+		"""Add new elements to lists of deja_vues"""
+		if sequences:
+			self.seqs_deja_vues.extend(vues)
+			self.seqs_deja_vues = list(set(self.seqs_deja_vues))
+		else:
+			self.taxids_deja_vues.extend(vues)
+			self.taxids_deja_vues = list(set(self.taxids_deja_vues))
+
+	def main(self, taxids):
+		"""Dynamic sequence download"""
+		sequences = []
+		seqids = self._search(taxids)
+		if seqids:
+			if len(seqids) >= 1000: # Only filter if more than 1000 mathces in genbank
+				print "Lots of sequences -- filtering ...."
+				while len(sequences) < self.nseqs:
+					downloaded = []
+					temp_taxid = self._findGenera(taxids)
+					if not temp_taxid:
+						break
+					seqids = self._search(temp_taxid)
+					print seqids
+					if seqids:
+						downloaded = self._download(seqids)
+						if not downloaded:
+							break
+						filtered = self._filterSequences(downloaded)
+						if filtered:
+							sequences.extend(filtered)
+				return sequences
+			else:
+				sequences = self._download(seqids)
+				if sequences:
+					return sequences
+				for _ in range(self.thoroughness+1, self.max_thoroughness+1):
+					seqids = self._search(taxids)
+					sequences = self._download(seqids)
+				return sequences
+		else:
+			return None
+
+## Functions
+def findBestGenes(namesdict, genedict, thoroughness, allrankids, minnseq = 1, minpwithseq = 0.5):
+	"""Return suitable genes for phylogeny by searching for matches in GenBank"""
+	alltipids = [namesdict[e]["txids"] for e in namesdict.keys()]
+	genes = []
+	for gene in genedict.keys():
+		print "Checking [{0}]".format(gene)
+		taxid = genedict[gene]["taxid"]
+		if int(taxid) in allrankids:
+			gene_bool = []
+			for tipids in alltipids:
+				sequence_downloader = SequenceDownloader(gene_names = genedict[gene]["names"],\
+					nseqs = 0, thoroughness = thoroughness, maxpn = 0, seedsize = 0, maxtrys = 0,\
+					mingaps = 0, minoverlap = 0, maxlen = 0, minlen = 0)
+				res = sequence_downloader._search(tipids)
+				gene_bool.append(len(res) >= minnseq)
+			pwithseq = float(sum(gene_bool))/len(alltipids)
+			if pwithseq > minpwithseq:
+				genes.append(gene)
+	return genes
 
 if __name__ == "__main__":
 	pass
