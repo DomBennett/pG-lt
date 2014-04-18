fc5fd3fc64d48681a6d489886e68fb5817415cec
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-04-18 13:08:04 +0100

    Simplified TerminationPipe

--- sys_tools.py
+++ sys_tools.py
@@ -9,7 +9,7 @@ import subprocess,threading,sys
 class TerminationPipe(object):
 	"""Adapted pG object : exectute programs."""
 	#Background process class
-	def __init__(self, cmd, timeout, silent=True):
+	def __init__(self, cmd, timeout):
 		self.cmd = cmd
 		self.timeout = timeout
 		self.process = None
@@ -17,37 +17,16 @@ class TerminationPipe(object):
 		self.failure = False
 		self.stderr = 'EMPTY'
 		self.stdout = 'EMPTY'
-		self.silent = silent
 	
-	def run(self, silent=None, changeDir=False):
+	def run(self, silent = False):
 		def silentTarget():
-			if sys.platform == 'win32':
-				if changeDir:
-					self.process = subprocess.Popen("requires\\" + self.cmd, stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE)
-				else:
-					self.process = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, shell=True, stderr=subprocess.PIPE)
-			else:
-				if changeDir:
-					self.process = subprocess.Popen("./requires/" + self.cmd, stdout=subprocess.PIPE,shell=True, stderr=subprocess.PIPE)
-				else:
-					self.process = subprocess.Popen(self.cmd, stdout=subprocess.PIPE,shell=True, stderr=subprocess.PIPE)
+			self.process = subprocess.Popen(self.cmd, stdout=subprocess.PIPE,\
+				shell=True, stderr=subprocess.PIPE)
 			self.output = self.process.communicate()
-		
 		def loudTarget():
-			if sys.platform == 'win32':
-				if changeDir:
-					self.process = subprocess.Popen("requires\\" + self.cmd, shell=False)
-				else:
-					self.process = subprocess.Popen(self.cmd, shell=False)
-			else:
-				if changeDir:
-					self.process = subprocess.Popen("./requires/" + self.cmd, shell=False)
-				else:
-					self.process = subprocess.Popen(self.cmd, shell=False)
+			self.process = subprocess.Popen(self.cmd, shell=False)
 			self.output=self.process.communicate()
-		
-		if silent: self.silent = silent
-		if self.silent:
+		if silent:
 			thread = threading.Thread(target=silentTarget)
 		else:
 			thread = threading.Thread(target=loudTarget)
