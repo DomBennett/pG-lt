40fe66ab6f0758d1d821da9654b988b5f019675b
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-05-13 16:04:16 +0100

    Added to download tests

--- tests/test_tools_download.py
+++ tests/test_tools_download.py
@@ -5,7 +5,81 @@ Tests for Download tools.
 import unittest
 import mpe.tools.download as dtools
 
-# Mock data
+## Dummy data and stubs
+
+# expected terms and term search results
+t1_term = '(txid1[PORGN]) OR txid2[PORGN] AND (("name1"[GENE]) \
+OR "name2"[GENE]) NOT predicted[TI] NOT genome[TI] NOT unverified[TI]'
+t2_term = '(txid1[PORGN]) OR txid2[PORGN] AND (("name1"[TI]) \
+OR "name2"[TI]) NOT predicted[TI] NOT genome[TI] NOT unverified[TI]'
+t3_term = '(txid1[PORGN]) OR txid2[PORGN] AND (("name1") \
+OR "name2") NOT predicted[TI] NOT shotgun[TI] NOT scaffold[TI] \
+NOT assembly[TI] NOT unverified[TI]'
+t1_search_res = {'Count':0}
+t2_search_res = {'Count':2, 'IdList':['seq1', 'seq2']}
+t3_search_res = {'Count':3, 'IdList':['seq1', 'seq2', 'seq3']}
+
+# Dummy seq records for download
+class dummy_Seq(object):
+	def __init__(self):
+		pass
+	def tostring(self):
+		# Just for parsing
+		return "A" * 500
+
+class dummy_SeqRecord(object):
+	def __init__(self, description, length = 500):
+		self.description = description
+		self.length = length
+		self.seq = dummy_Seq()
+	def __len__(self):
+		return self.length
+
+seq1 = dummy_SeqRecord(description = "A sequence of NAME1")
+seq2 = dummy_SeqRecord(description = "A sequence of NAME2")
+seq3 = [dummy_SeqRecord(description = "A sequence of NAME3"),\
+	dummy_SeqRecord(description = "A sequence of NAME4"),\
+	dummy_SeqRecord(description = "A sequence of NAME5"),\
+	dummy_SeqRecord(description = "A sequence of NAME1")]
+
+
+# Sequences -- just Ts and Fs -- for testing filter
+sequences = [True for i in range(80)]
+sequences.extend([False for i in range(20)])
+
+# Dependent stubs
+def dummy_eSearch(term, retStart=0, retMax=1, usehistory="n",\
+	db = "nucleotide"):
+	if term == t1_term:
+		return t1_search_res
+	if term == t2_term:
+		return t2_search_res
+	if term == t3_term:
+		return t3_search_res
+
+def dummy_eFetch(ncbi_id, db = "nucleotide"):
+	if ncbi_id == 'seq1':
+		return seq1
+	elif ncbi_id == 'seq2':
+		return seq2
+	elif ncbi_id == 'seq3':
+		return seq3
+	else:
+		# return all as list
+		return [seq1, seq2, seq3]
+
+def dummy_align(sequences):
+	return all (sequences)
+
+def dummy_checkAlignment(alignment, mingaps, minoverlap, minlen):
+	return alignment
+
+dtools.etools.eSearch = dummy_eSearch
+dtools.etools.eFetch = dummy_eFetch
+dtools.atools.align = dummy_align
+dtools.atools.checkAlignment = dummy_checkAlignment
+
+# downloader init variables
 taxids = ['1', '2']
 gene_names = ['name1', 'name2']
 nseqs = 100
@@ -17,48 +91,25 @@ minoverlap = 200
 maxtrys = 100
 maxlen = 2000
 minlen = 300
-downloader = dtools.Downloader (gene_names = gene_names,\
-			nseqs = nseqs, thoroughness = thoroughness,\
-			maxpn = maxpn, seedsize = seedsize, maxtrys \
-			= maxtrys, mingaps = mingaps, minoverlap = minoverlap,\
-			maxlen = maxlen, minlen = minlen)
-t1_term = '(txid1[PORGN]) OR txid2[PORGN] AND (("name1"[GENE]) \
-OR "name2"[GENE]) NOT predicted[TI] NOT genome[TI] NOT unverified[TI]'
-t1_search_res = {'Count':0}
-t2_term = '(txid1[PORGN]) OR txid2[PORGN] AND (("name1"[TI]) \
-OR "name2"[TI]) NOT predicted[TI] NOT genome[TI] NOT unverified[TI]'
-t2_search_res = {'Count':2, 'IdList':['seq1', 'seq2']}
-t3_term = '(txid1[PORGN]) OR txid2[PORGN] AND (("name1") \
-OR "name2") NOT predicted[TI] NOT shotgun[TI] NOT scaffold[TI] \
-NOT assembly[TI] NOT unverified[TI]'
-t3_search_res = {'Count':3, 'IdList':['seq1', 'seq2', 'seq3']}
-
-#Stubs
-def dummry_eSearch (term, retStart=0, retMax=1, usehistory="n",\
-	db = "nucleotide"):
-	if term == t1_term:
-		return t1_search_res
-	if term == t2_term:
-		return t2_search_res
-	if term == t3_term:
-		return t3_search_res
-def dummy_align (sequences):
-	pass
-def dummy_checkAlignment(alignment, mingaps, minoverlap, minlen):
-	return True
-dtools.etools.eSearch = dummry_eSearch
-dtools.atools.align = dummy_align
-dtools.atools.checkAlignment = dummy_checkAlignment
 
 class DownloadTestSuite(unittest.TestCase):
 
 	def setUp(self):
 		# mock Downloader instance
-		self.downloader = downloader
+		self.downloader = dtools.Downloader (gene_names = gene_names,\
+			nseqs = nseqs, thoroughness = thoroughness,\
+			maxpn = maxpn, seedsize = seedsize, maxtrys \
+			= maxtrys, mingaps = mingaps, minoverlap = minoverlap,\
+			maxlen = maxlen, minlen = minlen)
 		# expected search terms at different thoroughnesses
 		self.t1_term = t1_term
 		self.t2_term = t2_term
 		self.t3_term = t3_term
+		self.seqids = ['seq1', 'seq2', 'seq3']
+		self.seq1 = seq1
+		self.seq2 = seq2
+		self.seq3 = seq3
+		self.sequences = sequences
 		self.taxids = taxids
 
 	def test_downloader_private_buildsearchterm_thoroughness1(self):
@@ -81,6 +132,32 @@ class DownloadTestSuite(unittest.TestCase):
 		self.assertEqual([len(res1),len(res2),len(res3)], [2, 1, 0])
 
 	def test_downloader_private_filter(self):
+		# weeds out Falses from sequences, should not be any Falses
+		sequences = self.sequences[:]
+		res_filtered,res_downloaded = self.downloader._filter(sequences)
+		self.assertTrue(all(res_filtered))
+
+	def test_downloader_private_findgeneinseq(self):
+		pass
+
+	def test_downloader_private_parse(self):
+		# seq3, a list of SeqRecords of which the third is the right
+		#  size and contains no Ns
+		res = self.downloader._parse(self.seq3)
+		self.assertIsNotNone(res)
+
+	def test_downloader_private_download(self):
+		res = self.downloader._download(self.seqids)
+		self.assertEqual(len(res), 3)
+
+	def test_downloader_run(self):
+		# reset thoroughness and deja_vues
+		self.downloader.thoroughness = 1
+		self.downloader.deja_vues = []
+		res = self.downloader.run(self.taxids)
+		self.assertEqual(len(res), 3)
+
+	def test_findbestgenes(self):
 		pass
 
 if __name__ == '__main__':
