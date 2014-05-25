89851e2c23a5ce336d71076238457bdbb3a890eb
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-05-25 21:03:26 +0100

    Completed unit tests for names tools

--- tests/test_tools_names.py
+++ tests/test_tools_names.py
@@ -2,5 +2,76 @@
 Tests for Names tools.
 """
 
-import unittest,pickle,os
+import unittest,pickle,os,json
 import mpe.tools.names as ntools
+import taxon_names_resolver as tnr
+
+ntools.etools.Entrez.email = "python.unittests@mpe.program"
+
+## Test data
+with open(os.path.join('data','test_search.json'),\
+	'r') as file:
+	res = json.load(file)
+
+with open(os.path.join('data', 'test_namesdict.p'),\
+	'r') as file:
+	exp_namesdict = pickle.load(file)
+
+terms = ['GenusA speciesA', 'GenusA speciesB', 'GenusA speciesC',\
+'GenusB speciesD','GenusB speciesE','GenusC speciesF','GenusD \
+speciesG','GenusE speciesH','GenusF speciesI','GenusG speciesJ']
+exp_allrankids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,\
+15, 16, 17, 21, 22, 23, 24, 25, 26, 31, 32, 33, 34, 35, 41, 42, 43,\
+44, 51]
+
+## Stubs
+def dummy_eFetch(taxid, db):
+	return [{'ParentTaxId':1}]
+
+
+def dummy_findChildren(taxid, next):
+	return [60]
+
+
+class Dummy_GnrDataSources(object):
+	def __init__(self):
+		pass
+	def byName(self, names, invert = False):
+		if invert:
+			return [1, 2, 3]
+		else:
+			return [4]
+
+class NamesToolsTestSuite(unittest.TestCase):
+
+	def setUp(self):
+		self.True_GnrDataSources = tnr.gnr_tools.GnrDataSources
+		self.true_eFetch = ntools.etools.eFetch
+		self.true_findChildren = ntools.etools.findChildren
+		tnr.gnr_tools.GnrDataSources = Dummy_GnrDataSources
+		ntools.etools.eFetch = dummy_eFetch
+		ntools.etools.findChildren = dummy_findChildren
+		self.resolver = tnr.resolver.Resolver(terms = terms, taxon_id\
+			= 51, verbose = False)
+		test_store = tnr.gnr_tools.GnrStore(terms)
+		test_store.add(res)
+		self.resolver._store = test_store
+
+	def tearDown(self):
+		tnr.gnr_tools.GnrDataSources = self.True_GnrDataSources
+		ntools.etools.eFetch = self.true_eFetch
+		ntools.etools.findChildren = self.true_findChildren
+
+	def test_gennamesdict(self):
+		namesdict,allrankids = ntools.genNamesDict(self.resolver)
+		self.assertEqual(allrankids, exp_allrankids)
+		self.assertEqual(namesdict, exp_namesdict)
+
+	def test_gentaxtree(self):
+		tree,line = ntools.genTaxTree(self.resolver, exp_namesdict,\
+			draw = False)
+		self.assertTrue(tree)
+		self.assertEqual(line, [u'51'])
+
+if __name__ == '__main__':
+    unittest.main()
