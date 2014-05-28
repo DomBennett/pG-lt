6a98dadb033632e61e5c5305826aa2a3bbdeebd7
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-05-28 17:01:59 +0100

    Updated MPE and system tools
    Added description and verbose arg

--- MPE.py
+++ MPE.py
@@ -28,94 +28,47 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
-import argparse,pickle,csv,sys,os
+
+## Packages
+import argparse
 from mpe.tools.system import Stager
-from mpe import _PARS as default_pars
-from mpe import _GPARS as default_gpars
+from mpe.tools.system import readArgs
 
-def main():
-	args = parser.parse_args()
-	print args
-	if args.names:
-		try:
-			terms = []
-			with open(args.names) as names:
-				for name in names:
-					terms.append(name.strip())
-			terms = [term for term in terms if not term == '']
-		except IOError:
-			print "Names file could not be opened. File: \
-[{0}]".format(args.names)
-			sys.exit()
-	else:
-		print "No names file given! Type 'MPE.py -h' for help."
-		sys.exit()
-	if args.parameters:
-		parameters = args.parameters
-	else:
-		parameters = default_pars
-	if args.genes:
-		genes = args.genes
-	else:
-		genes = default_gpars
-	try:
-	 	genedict = {}
-	 	with open(genes, 'rb') as csvfile:
-	 		reader = csv.DictReader(csvfile)
-	 		for row in reader:
-	 			temp = {}
-	 			temp["names"] = row["names"].split(":")
-	 			temp["taxid"] = row["taxid"]
-	 			temp["mingaps"] = row["mingaps"]
-	 			temp["minoverlap"] = row["minoverlap"]
-	 			temp["minfails"] = row["minfails"]
-	 			temp["maxtrys"] = row["maxtrys"]
-	 			temp["minseedsize"] = row["minseedsize"]
-	 			temp["maxseedtrys"] = row["maxseedtrys"]
-	 			genedict[row['gene']] = temp
-	except IOError:
-		print "Gene parameters file could not be opened. File: [{0}]".genes
-		sys.exit()
-	except KeyError:
-		print "Unexpected headers in gene parameters."
-		sys.exit()
-	try:
-	 	paradict = {}
-	 	with open(parameters, 'rb') as csvfile:
-	 		reader = csv.DictReader(csvfile)
-	 		for row in reader:
-	 			paradict[row["Parameter"]] = row["Value"]
-	except IOError:
-		print "Parameters file could not be opened. File: [{0}]".parameters
-	 	sys.exit()
-	# Output
- 	with open(".genedict.p", "wb") as file:
- 		pickle.dump(genedict, file)
- 	with open(".paradict.p", "wb") as file:
- 		pickle.dump(paradict, file)
- 	with open(".terms.p", "wb") as file:
- 		pickle.dump(terms, file)
- 	if args.restart:
-	 	# Find current stage
-	 	if os.path.isfile('.stage.p'):
-	 		with open('.stage.p', 'rb') as file:
-	 			pickle.load(file)
-	 	else:
-	 		stage = 0
-	 	if stage < 4:
-			Stager.run_all(stage)
-		else:
-			print "All stages have already been run."
-	else:
-		Stager.run_all(0)
+## Variables
+description = """
+MPE (D.J. Bennett (C) 2014)
 
+A pipeline for the automated generation of phylogenies from taxonomic
+names through Mass Phylogeny Estimation.
+"""
+
+##TODO: python interaction
+
+## Terminal interaction
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description="Mass Phylogeny Estimation (MPE) -\
-an automated pipeline for the generation of phylogenies from taxonomic names (D.J.\
- Bennett (C) 2014).")
+	parser = argparse.ArgumentParser(description=description)
 	parser.add_argument("-names", "-n", help=".txt file of taxonomic names.")
 	parser.add_argument("-parameters", "-p", help=".csv file of parameters.")
 	parser.add_argument("-genes", "-g", help=".csv file of gene parameters")
+	parser.add_argument("--verbose", help="increase output verbosity",
+					action="store_true")
 	parser.add_argument("-restart", "-r", action='store_true', default = False,\
 		help="restart process from last completed stage")
-	main()
+	# Read args
+	args = parser.parse_args()
+	stage = readArgs(args)
+	# Print + run
+	if args.parameters:
+		parameters = args.parameters
+	else:
+		parameters = 'DEFAULT'
+	if args.genes:
+		genes = args.genes
+	else:
+		genes = 'DEFAULT'
+	print '\n' + description + '\n'
+	print 'Using ...'
+	print '.... [{0}] names'.format(args.names)
+	print '.... [{0}] parameters'.format(parameters)
+	print '.... [{0}] genes\n'.format(genes)
+	Stager.run_all(stage = stage, verbose = args.verbose)
--- mpe/tools/system.py
+++ mpe/tools/system.py
@@ -6,14 +6,18 @@ MPE system tools
 """
 
 ## Packages
-import subprocess,threading,sys,os,platform
+import subprocess,threading,sys,os,platform,csv,pickle,argparse
 from datetime import datetime
 from mpe import stages
+from mpe import _PARS as default_pars
+from mpe import _GPARS as default_gpars
 
+## Classes
 class StageError(Exception):
 	pass
 
 class Tee(object):
+	"""Tee class: duplicate stdout"""
 	#http://stackoverflow.com/questions/616645/how-do-i-duplicate-sys-stdout-to-a-log-file-in-python/616686#616686
 	def __init__(self, path):
 		self.file = open(path, 'w')
@@ -34,11 +38,12 @@ class Tee(object):
 		self.stdout.flush()
 
 class Stager(object):
-	"""Stager class : runs each file in stage folder.\
-Adapted from code written by L. Hudson."""
+	"""Stager class : runs each file in stage folder. Adapted from\
+ code written by L. Hudson."""
 	STAGES = stages.STAGES
 
-	def __init__(self, stage):
+	def __init__(self, stage, verbose = True):
+		self.verbose = verbose
 		if stage not in self.STAGES:
 			raise StageError('Stage [{0}] not recognised'.format(stage))
 		else:
@@ -46,47 +51,53 @@ Adapted from code written by L. Hudson."""
 			output_dir = os.path.split(self.STAGES[stage])[1]
 			self.output_dir = os.path.splitext(output_dir)[0]
 
-	def _prime(self):
-		# Creates self.output_dir. An error is raised if it already exists.
-		if not os.path.isdir(self.output_dir):
-			os.mkdir(self.output_dir)
-		return Tee(os.path.join(self.output_dir, 'log.txt'))
-
 	def _time_string(self):
 		return datetime.today().strftime("%A, %d %B %Y %I:%M%p")
 
-	def _cmd(self, arg):
-		s = subprocess.Popen(['python', '-u', arg], stdout=subprocess.PIPE,
-			stderr=subprocess.STDOUT)
-		while s.poll() is None:
-			line = s.stdout.readline()
-			if len(line) > 1:
-				print line[:-1]
-			else:
-				print line
-		if s.poll():
-			sys.exit(s.poll())
+	def _cmd(self, arg, duplicate = True):
+		if duplicate:
+			with Tee(os.path.join(self.output_dir, 'log.txt')):
+				# -u let's prints as the process runs
+				s = subprocess.Popen(['python', '-u', arg],\
+					stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+				while s.poll() is None:
+					line = s.stdout.readline()
+					print line[:-1]
+				if s.poll():
+					sys.exit(s.poll())
+		else:
+			with open(os.path.join(self.output_dir, 'log.txt'), 'w')\
+			as logfile:
+				s = subprocess.Popen(['python', '-u', arg],\
+					stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+				while s.poll() is None:
+					line = s.stdout.readline()
+					logfile.write(line)
+				if s.poll():
+					sys.exit(s.poll())
 
 	def run(self):
-		with self._prime():
-			print 'Stage [{0}] started at [{1}]'.format(self.stage,
+		if not os.path.isdir(self.output_dir):
+			os.mkdir(self.output_dir)
+		print '\nStage [{0}] started at [{1}]'.format(self.stage,
 			self._time_string())
-			print 'Running on [{0}] [{1}]'.format(platform.node(),
-				platform.platform())
-			pyfile = self.STAGES[self.stage]
-			self._cmd('--version')
-			self._cmd(pyfile)
-			print 'Stage finished at [{0}]'.format(self._time_string())
+		print ' .... working .... '
+		stagefile = self.STAGES[self.stage]
+		self._cmd(stagefile, duplicate = self.verbose)
+		print 'Stage finished at [{0}]\n'.format(self._time_string())
 
 	@classmethod
-	def run_all(klass, stage):
+	def run_all(klass, stage, verbose = True):
+		# print system info
+		print 'Running on [{0}] [{1}]'.format(platform.node(),
+				platform.platform())
+		# print python verion
+		print 'Python [{0}]'.format(sys.version)
 		for s in sorted(Stager.STAGES.keys()[stage:]):
-			Stager(s).run()
-
+			Stager(s, verbose).run()
 
 class TerminationPipe(object):
-	"""Adapted pG object : exectute programs."""
-	#Background process class
+	"""Adapted pG object : exectute background programs."""
 	def __init__(self, cmd, timeout = 99999, silent = True):
 		self.cmd = cmd
 		self.timeout = timeout
@@ -115,3 +126,76 @@ class TerminationPipe(object):
 			self.process.terminate()
 			thread.join()
 			self.failure = True
+
+## Functions
+def readArgs(args):
+	"""Safely read arguments write pickle files, return stage"""
+	if args.names:
+		try:
+			terms = []
+			with open(args.names) as names:
+				for name in names:
+					terms.append(name.strip())
+			terms = [term for term in terms if not term == '']
+		except IOError:
+			print "Names file could not be opened. File: \
+[{0}]".format(args.names)
+			sys.exit()
+	else:
+		print "No names file given! Type 'MPE.py -h' for help."
+		sys.exit()
+	if args.parameters:
+		parameters = args.parameters
+	else:
+		parameters = default_pars
+	if args.genes:
+		genes = args.genes
+	else:
+		genes = default_gpars
+	genedict = {}
+	try:
+		with open(genes, 'rb') as csvfile:
+			reader = csv.DictReader(csvfile)
+			for row in reader:
+				temp = {}
+				temp["names"] = row["names"].split(":")
+				temp["taxid"] = row["taxid"]
+				temp["mingaps"] = row["mingaps"]
+				temp["minoverlap"] = row["minoverlap"]
+				temp["minfails"] = row["minfails"]
+				temp["maxtrys"] = row["maxtrys"]
+				temp["minseedsize"] = row["minseedsize"]
+				temp["maxseedtrys"] = row["maxseedtrys"]
+				genedict[row['gene']] = temp
+	except IOError:
+		print "Gene parameters file could not be opened. File: [{0}]".genes
+		sys.exit()
+	except KeyError:
+		print "Unexpected headers in gene parameters."
+		sys.exit()
+	paradict = {}
+	try:
+		with open(parameters, 'rb') as csvfile:
+			reader = csv.DictReader(csvfile)
+			for row in reader:
+				paradict[row["Parameter"]] = row["Value"]
+	except IOError:
+		print "Parameters file could not be opened. File: [{0}]".parameters
+		sys.exit()
+	# Output
+	with open(".genedict.p", "wb") as file:
+		pickle.dump(genedict, file)
+	with open(".paradict.p", "wb") as file:
+		pickle.dump(paradict, file)
+	with open(".terms.p", "wb") as file:
+		pickle.dump(terms, file)
+	stage = 0
+	if args.restart:
+		# Find current stage
+		if os.path.isfile('.stage.p'):
+			with open('.stage.p', 'rb') as file:
+				pickle.load(file)
+		if stage > 4:
+			print "All stages have already been run!"
+			sys.exit()
+	return stage
