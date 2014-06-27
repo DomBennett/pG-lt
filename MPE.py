6868b1d612c6385245f4fecbf77cfd9e8fb49e6d
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-06-27 18:52:25 +0100

    Now takes folders not text files

--- MPE.py
+++ MPE.py
@@ -1,8 +1,9 @@
 #! /bin/usr/env python
 ## D.J. Bennett
 ## 26/05/2014
+##TODO: bring back restart
 """
-MPE is a pipeline for the automated generation of phylogenies through 'Mass
+mpe is a pipeline for the automated generation of phylogenies through 'Mass
 Phylogeny Estimation'. This program is built on top of phyloGenerator (C) 2013
 and was written by D.J. Bennett with additional help from W.D. Pearse and L. Hudson.
 This program makes use of external programs for phylogeny generation and bioinformatics
@@ -30,32 +31,292 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 """
 
 ## Packages
-import argparse
+import argparse,pickle,sys,csv,os,re,logging,platform
+from datetime import datetime
 from mpe.tools.system import Stager
-from mpe.tools.system import readArgs
+from mpe import _PARS as default_pars_file
+from mpe import _GPARS as default_gpars_file
 
+## Informative messages
+ioerror_msg = "[{0}] file could not be opened in [{1}]. Check that \
+it is not opened by another program"
+nonamestxt_msg = 'No folders containing \'names.txt\' files found! \
+All taxonomic names should be placed in subdirectories and called: \
+\'names.txt\''
 description = """MPE D.J. Bennett (C) 2014
 
 A pipeline for the automated generation of phylogenies from taxonomic
 names through Mass Phylogeny Estimation."""
 
-def parseArgs():
-	"""Read arguments"""
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-names", "-n", help=".txt file of taxonomic names.")
-	parser.add_argument("-parameters", "-p", help=".csv file of parameters.")
-	parser.add_argument("-genes", "-g", help=".csv file of gene parameters")
-	parser.add_argument("-restart", "-r", help="restart from specified stage")
-	parser.add_argument("--verbose", help="increase output verbosity",
-					action="store_true")
-	parser.add_argument("--development", help="log warnings (developer only)",
-					action="store_true")
-	return parser
+## Classes
+class PrimingError(Exception):
+	pass
 
-if __name__ == '__main__':
+## Functions
+def printHeader():
+	"""Print a nice program description header"""
+	# use 70 cols as I think this is standard
 	print '\n' + '#' * 70
 	print description
 	print '#' * 70 + '\n'
+
+def logPars(paradict):
+	"""Print all mpe parameters"""
+	logging.info('\nUsing the following parameters:')
+	for key in paradict.keys():
+		logging.info('    [{0}] = [{1}]'.format(key, paradict[key]))
+
+def logGpars(genedict):
+	"""Print gene parameters"""
+	logging.info('\nUsing the following genes and gene parameters:')
+	for gene in genedict.keys():
+		logging.info('  Gene: [{0}]'.format(gene))
+		for par in genedict[gene]:
+			logging.info('    [{0}] = [{1}]'.format(par,\
+				genedict[gene][par]))
+
+def prime(directory, arguments):
+	"""Write pickle files, print arguments"""
+	# Write pickle files
+	with open(os.path.join(directory, ".genedict.p"),\
+		"wb") as file:
+		pickle.dump(arguments['genedict'], file)
+	with open(os.path.join(directory,".paradict.p"),\
+		"wb") as file:
+		pickle.dump(arguments['paradict'], file)
+	with open(os.path.join(directory,".terms.p"),\
+		"wb") as file:
+		pickle.dump(arguments['terms'], file)
+	# Print arguments
+	logging.info('Working with [{0}] names'.format(len(arguments['terms'])))
+	logPars(arguments['paradict'])
+	logGpars(arguments['genedict'])
+
+def parseArgs():
+	"""Read command-line arguments"""
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-email", "-e", help="please provide email for NCBI")
+	parser.add_argument("-restart", "-r", help="restart from specified stage")
+	parser.add_argument("--verbose", help="increase output verbosity",
+					action="store_true")
+	parser.add_argument("--debug", help="log warnings (developer only)",
+					action="store_true")
+	return parser
+
+def readInNames(directory):
+	"""Read names from text file in dir"""
+	terms = []
+	with open(os.path.join(directory, 'names.txt')) as names:
+		for name in names:
+			terms.append(name.strip())
+	terms = [term for term in terms if not term == '']
+	return terms
+
+def readInGenePars(gpars_file):
+	"""Read gene_parameters.csv. Return list of dictionaries."""
+	def split(element):
+		# split elements by ':'
+		if ':' in element:
+			return element.split(':')
+		return element
+	def _read(gpars_file, template, genes = None):
+		# open csv file and replace parameters in template
+		#  if they are None. If genes specified, only read
+		#  rows for those genes.
+		with open(gpars_file, 'rb') as csvfile:
+			reader = csv.DictReader(csvfile)
+			for row in reader:
+				if genes:
+					if not row['gene'] in genes:
+						continue
+				temp = template.copy()
+				for key in temp.keys():
+					if row[key]:
+						if temp[key] is None:
+							temp[key] = split(row[key])
+				genedict[row['gene']] = temp
+		return genedict
+	# check if file exists, else use default
+	if not os.path.isfile(gpars_file):
+		return readInGenePars(default_gpars_file)
+	# genedicts
+	genedict = {}
+	# template of dict in genedict
+	template = {'names' : None, 'taxid' : None,\
+	'mingaps' : None, 'minoverlap' : None, 'minfails'\
+	: None, 'maxtrys' : None, 'minseedsize' : None,\
+	'maxseedtrys': None}
+	# open file, read each row and fill in template
+	genedict = _read(gpars_file, template)
+	# if Nones, use defaults
+	nones = False
+	for gene in genedict.keys():
+		for par in genedict[gene].keys():
+			if genedict[gene][par] is None:
+				nones = True
+				break
+	if nones:
+		# run _read for defaults and limit to genes in genedict
+		genedict = _read(default_gpars_file, template, genedict.keys())
+	return genedict
+
+def readInPars(pars_file):
+	"""Read gene_parameters.csv. Return dictionary."""
+	def _read(pars_file, paradict):
+		# open csv, and replace all Nones
+		with open(pars_file, 'rb') as csvfile:
+			reader = csv.DictReader(csvfile)
+			for row in reader:
+				if paradict[row["Parameter"]] is None:
+					paradict[row["Parameter"]] = row["Value"]
+		return paradict
+	# check if file exists, else use default
+	if not os.path.isfile(pars_file):
+		return readInPars(default_pars_file)
+	# template
+	paradict = {'nseqs' : None, 'naligns' : None,\
+	'ntrees' : None, 'download_thoroughness' : None,\
+	'maxlen' : None, 'minlen' : None, 'maxtrys' : None,\
+	'maxpedge': None}
+	# open file, read each row, extract value
+	paradict = _read(pars_file, paradict)
+	# if Nones remain, use default
+	nones = False
+	for key in paradict.keys():
+		if paradict[key] is None:
+			nones = True
+			break
+	if nones:
+		paradict = _read(default_pars_file, paradict)
+	return paradict
+
+def sortArgs(directory, email):
+	"""Search for relevant files in dir, return list of arguments"""
+	# find text file and read, raise error if fail
+	try:
+		terms = readInNames(directory)
+	except IOError:
+		logging.error(ioerror_msg.format('names.txt', directory))
+		raise PrimingError()
+	# find gene parameter file and read, raise error if fail
+	try:
+		genedict = readInGenePars(os.path.join(directory, 'gene_parameters.csv'))
+	except IOError:
+		logging.error(ioerror_msg.format('gene_parameters.csv', directory))
+		raise PrimingError()
+	# find parameter file and read, raise error if fail
+	try:
+		paradict = readInPars(os.path.join(directory, 'parameters.csv'))
+	except IOError:
+		logging.error(ioerror_msg.format('parameters.csv', directory))
+		raise PrimingError()
+	# add email to paradict
+	paradict['email'] = email
+	return {'terms' : terms, 'genedict' : genedict, 'paradict' : paradict}
+
+def getDirs():
+	"""Return folders in directory with names.txt files"""
+	# list all folders
+	unchecked_dirs = [f for f in os.listdir('.') if not\
+	os.path.isfile(f)]
+	# remove hidden folders
+	unchecked_dirs = [d for d in unchecked_dirs if not\
+	re.match('^\.', d)]
+	# loop through each and check they contain a names.txt
+	checked_dirs = []
+	for each in unchecked_dirs:
+		path = os.path.join (os.getcwd(), each)
+		files = os.listdir(path)
+		if 'names.txt' in files:
+			checked_dirs.append(each)
+	if len(checked_dirs) > 0:
+		return checked_dirs
+	else:
+		logging.error(nonamestxt_msg)
+		sys.exit()
+
+def setUpLogging(verbose, debug):
+	"""Set up logging : direct and control log statements"""
+	# root logger
+	root_log = logging.getLogger()
+	if debug:
+		# log all statements above DEBUG level
+		root_log.setLevel(logging.DEBUG)
+	else:
+		# log all statements above INFO level
+		# (which is higher than DEBUG)
+		root_log.setLevel(logging.INFO)
+	# add file hander to root
+	logfile = os.path.join(os.getcwd(), 'log.txt')
+	loghandler = logging.FileHandler(logfile, 'a')
+	# set statement format -- I only want the message
+	loghandler.setFormatter(logging.Formatter('%(message)s'))
+	root_log.addHandler(loghandler)
+	if verbose:
+		# if verbose, copy all info statements to console
+		console = logging.StreamHandler()
+		console.setFormatter(logging.Formatter('%(message)s'))
+		root_log.addHandler(console)
+
+def logStartFolder(directory):
+	"""Log start of new folder"""
+	logging.info('\n' + '#' * 70 + '\n')
+	logging.info('Folder [{0}] started at [{1}]'.format(directory,
+		datetime.today().strftime("%A, %d %B %Y %I:%M%p")))
+
+def logEndFolder(directory):
+	"""Log end of folder"""
+	logging.info('Folder [{0}] finished at [{1}]'.format(directory,
+		datetime.today().strftime("%A, %d %B %Y %I:%M%p")))
+
+def logStart(directories):
+	"""Log end"""
+	logging.info('\n' + '#' * 70)
+	logging.info(description)
+	logging.info('#' * 70 + '\n')
+	logging.info('Running on [{0}] [{1}]'.format(platform.node(),
+				platform.platform()))
+	logging.info('Python [{0}]\n'.format(sys.version))
+	logging.info('Working with the following directories:')
+	# convert dirs to string
+	print_dirs = ''
+	for each in directories[:-1]:
+		print_dirs += each + ', '
+	print_dirs += directories[-1]
+	logging.info('[{0}]'.format(print_dirs))
+
+def main():
+	"""Run mpe with user defined arguments from command-line"""
+	# print program header
+	printHeader()
+	# read arguments
 	parser = parseArgs()
-	stage,verbose,dev = readArgs(parser.parse_args())
-	Stager.run_all(stage = stage, verbose = verbose, dev = dev)
+	args = parser.parse_args()
+	if not args.email:
+		# stop if no email
+		print 'An email address MUST be provided. Use \'-e\'.'
+		sys.exit()
+	# set up logging
+	setUpLogging(args.verbose, args.debug)
+	# search cwd for folders that contain names and parameter files
+	dirs = getDirs()
+	logStart(dirs)
+	# loop through each folder
+	for each in dirs:
+		if not args.verbose:
+			print 'Woking on [{0}]'.format(each)
+		logStartFolder(each)
+		try:
+			# get list of arguments
+			arguments = sortArgs(each, args.email)
+			# initialise hidden files
+			prime(each, arguments)
+			# run Stager
+			Stager.run_all(each, stage = 0, verbose = args.verbose)
+		except:
+			logging.error('An error occurred for [{0}]! Check log \
+file for more details. Moving to next folder.'.format(each))
+		logEndFolder(each)
+
+if __name__ == '__main__':
+	main()
--- mpe/tools/system.py
+++ mpe/tools/system.py
@@ -2,24 +2,14 @@
 ## D.J. Bennett
 ## 24/03/2014
 """
-MPE system tools
+mpe system tools
 """
 
 ## Packages
-import subprocess,threading,sys,os,platform,csv,pickle,logging
+import subprocess,threading,os,logging
 from datetime import datetime
-from mpe import _PARS as default_pars
-from mpe import _GPARS as default_gpars
 
 ## Classes
-class LogFilter(object):
-	#http://stackoverflow.com/questions/8162419/python-logging-specific-level-only
-    def __init__(self, level):
-        self.__level = level
-
-    def filter(self, logRecord):
-        return logRecord.levelno <= self.__level
-
 class StageError(Exception):
 	pass
 
@@ -27,78 +17,44 @@ class Stager(object):
 	"""Stager class : runs each file in stage folder. Adapted from\
  code written by L. Hudson."""
 
-	def __init__(self, stage, verbose = True, dev = False):
-		self.verbose = verbose
-		self.dev = dev
+	def __init__(self, wd, stage):
 		if stage not in self.STAGES:
 			raise StageError('Stage [{0}] not recognised'.format(stage))
 		else:
+			self.wd = wd
 			self.stage = stage
 			# dir is second element of tuple
-			self.output_dir = self.STAGES[stage][1]
-
-	def _prime(self):
-		if not os.path.isdir(self.output_dir):
-			os.mkdir(self.output_dir)
-		# root logger
-		root_log = logging.getLogger()
-		root_log.setLevel(logging.INFO)
-		# add file hander to root
-		logfile = os.path.join(self.output_dir, 'log.txt')
-		loghandler = logging.FileHandler(logfile, 'a')
-		loghandler.setFormatter(logging.Formatter('%(message)s'))
-		root_log.addHandler(loghandler)
-		if self.verbose:
-			# if verbose
-			console = logging.StreamHandler()
-			console.setFormatter(logging.Formatter('%(message)s'))
-			root_log.addHandler(console)
-		# if not dev, filter out everything but info
-		if not self.dev:
-			root_log.addFilter(LogFilter(logging.INFO))
-
-	def _second(self):
-		root_log = logging.getLogger()
-		root_handlers = root_log.handlers[:]
-		# remove all handlers
-		for each in root_handlers:
-			root_log.removeHandler(each)
+			self.output_dir = os.path.join(wd, self.STAGES[stage][1])
 
 	def _start(self):
-		logging.info('\n' + '#' * 70)
-		logging.info('Stage [{0}] started at [{1}]'.format(self.stage,
+		logging.info('\nStage [{0}] started at [{1}]'.format(self.stage,
 			self._time_string()))
-		logging.info('Running on [{0}] [{1}]'.format(platform.node(),
-					platform.platform()))
-		logging.info('Python [{0}]'.format(sys.version))
-		logging.info('#' * 70 + '\n')
 
 	def _end(self):
-		logging.info('\n' + '#' * 70)
 		logging.info('Stage [{0}] finished at [{1}]'.format(self.stage,
 			self._time_string()))
-		logging.info('#' * 70 + '\n')
 
 	def _time_string(self):
 		return datetime.today().strftime("%A, %d %B %Y %I:%M%p")
 
 	def _cmd(self):
 		# function is first element of tuple
-		self.STAGES[self.stage][0]()
+		# pass working directory to it
+		self.STAGES[self.stage][0](self.wd)
 
 	def run(self):
-		self._prime() # add handlers to root logger
+		if not os.path.isdir(self.output_dir):
+			os.mkdir(self.output_dir)
 		self._start() # log system info
 		self._cmd() # run stage
 		self._end() # log end time
-		self._second() # remove handlers from root logger
 
 	@classmethod
-	def run_all(klass, stage, verbose = True, dev = False):
+	def run_all(klass, wd, stage, verbose):
 		for s in sorted(Stager.STAGES.keys()[stage:]):
 			if not verbose:
-				print 'Stage [{0}]'.format(Stager.STAGES[s][1])
-			Stager(s, verbose, dev).run()
+				print '  Stage [{0}]'.format(Stager.STAGES[s][1])
+			Stager(wd, s).run()
 
 class TerminationPipe(object):
 	"""Adapted pG object : exectute background programs."""
@@ -130,86 +86,3 @@ class TerminationPipe(object):
 			self.process.terminate()
 			thread.join()
 			self.failure = True
-
-## Functions
-def readArgs(args):
-	"""Safely read arguments write pickle files, return stage and verbosity"""
-	if args.restart:
-		try:
-			stage = int(args.restart) - 1
-		except TypeError:
-			print "Restart must be numeric!"
-			sys.exit()
-		print "Restarting from stage: [{0}]\n".format(args.restart)
-		return stage,args.verbose,args.development
-	if args.names:
-		try:
-			terms = []
-			with open(args.names) as names:
-				for name in names:
-					terms.append(name.strip())
-			terms = [term for term in terms if not term == '']
-		except IOError:
-			print "Names file could not be opened. File: \
-[{0}]".format(args.names)
-			sys.exit()
-	else:
-		print "No names file given! Type 'MPE.py -h' for help."
-		sys.exit()
-	if args.parameters:
-		parameters = args.parameters
-	else:
-		parameters = default_pars
-	if args.genes:
-		genes = args.genes
-	else:
-		genes = default_gpars
-	genedict = {}
-	try:
-		with open(genes, 'rb') as csvfile:
-			reader = csv.DictReader(csvfile)
-			for row in reader:
-				temp = {}
-				temp["names"] = row["names"].split(":")
-				temp["taxid"] = row["taxid"]
-				temp["mingaps"] = row["mingaps"]
-				temp["minoverlap"] = row["minoverlap"]
-				temp["minfails"] = row["minfails"]
-				temp["maxtrys"] = row["maxtrys"]
-				temp["minseedsize"] = row["minseedsize"]
-				temp["maxseedtrys"] = row["maxseedtrys"]
-				genedict[row['gene']] = temp
-	except IOError:
-		print "Gene parameters file could not be opened. File: [{0}]".genes
-		sys.exit()
-	except KeyError:
-		print "Unexpected headers in gene parameters."
-		sys.exit()
-	paradict = {}
-	try:
-		with open(parameters, 'rb') as csvfile:
-			reader = csv.DictReader(csvfile)
-			for row in reader:
-				paradict[row["Parameter"]] = row["Value"]
-	except IOError:
-		print "Parameters file could not be opened. File: [{0}]".parameters
-		sys.exit()
-	# Output
-	with open(".genedict.p", "wb") as file:
-		pickle.dump(genedict, file)
-	with open(".paradict.p", "wb") as file:
-		pickle.dump(paradict, file)
-	with open(".terms.p", "wb") as file:
-		pickle.dump(terms, file)
-	# Print
-	if args.parameters:
-		parameters = args.parameters
-	else:
-		parameters = 'DEFAULT'
-	if args.genes:
-		genes = args.genes
-	else:
-		genes = 'DEFAULT'
-	print 'Running with names [{0}], parameters [{1}] and gene \
-parameters [{2}]'.format(args.names, parameters, genes)
-	return 0,args.verbose,args.development
