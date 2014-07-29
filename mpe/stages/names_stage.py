4cba970164ef9eef525384f71842aba9174718a6
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-07-29 12:40:37 +0100

    Updated logged statements

--- mpe/stages/names_stage.py
+++ mpe/stages/names_stage.py
@@ -21,7 +21,7 @@ incorrect names resolution.'
 
 def run(wd = os.getcwd()):
 	## print stage
-	logging.info("\nNames resolution\n")
+	logging.info("Names resolution\n")
 
 	## Dirs
 	names_dir = os.path.join(wd, '1_names')
@@ -42,7 +42,8 @@ def run(wd = os.getcwd()):
 	minspecies = 5
 
 	## Process
-	logging.info('Searching for taxids')
+	logging.info('Searching for taxids....')
+	logging.info('------TaxonNamesResolver:Start------')
 	try:
 		parentid = int(paradict["parentid"])
 	except:
@@ -52,13 +53,14 @@ def run(wd = os.getcwd()):
 	resolver = Resolver(terms = terms, datasource = "NCBI",\
 		taxon_id = parentid)
 	resolver.main()
-	logging.info("Generating names dictionary")
+	logging.info('------TaxonNamesResolver:End------')
+	logging.info("Generating names dictionary....")
 	try:
 		namesdict,allrankids = ntools.genNamesDict(resolver)
 	except ntools.TaxonomicRankError:
 		print error_msg
 		sys.exit()
-	logging.info('Generating taxonomic tree')
+	logging.info('Generating taxonomic tree....')
 	taxontree,shared_lineages = ntools.genTaxTree(resolver,\
 		namesdict)
 
@@ -77,5 +79,5 @@ def run(wd = os.getcwd()):
 		"taxontree.tre"), "newick")
 
 	## Finish message
-	logging.info('Stage finished. Resolved [{0}] names \
+	logging.info('Resolved [{0}] names \
 including outgroup.'.format(len(namesdict.keys())))
