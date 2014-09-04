fb48dce22818fe23c016eb270dd6b79cdf73f33e
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-09-04 18:11:47 +0100

    Tidying up code

--- mpe/tools/download_tools.py
+++ mpe/tools/download_tools.py
@@ -14,8 +14,8 @@ from Bio.SeqFeature import SeqFeature
 ## Objects
 class Downloader(object):
 	"""Download sequences given taxids and gene_names"""
-	def __init__(self, gene_names, nseqs, thoroughness, maxpn, seedsize, maxtrys, \
-			mingaps, minoverlap, maxlen, minlen):
+	def __init__(self, gene_names, nseqs, thoroughness, maxpn,\
+		seedsize, maxtrys, mingaps, minoverlap, maxlen, minlen):
 		self.gene_names = gene_names
 		self.nseqs = nseqs
 		self.max_thoroughness = thoroughness
@@ -31,40 +31,46 @@ class Downloader(object):
 		self.pattern = re.compile("[ACTGactg]")
 
 	def _buildSearchTerm(self, taxids, thoroughness):
-		"""Generate NCBI GenBank query given taxids, gene_names and thoroughness"""
+		"""Generate NCBI GenBank query given taxids, gene_names and
+thoroughness"""
 		taxids_term = "txid{0}[PORGN]".format(taxids[0])
 		for taxid in taxids[1:]:
 			taxids_term = ("({0}) OR txid{1}[PORGN]".\
 						 format(taxids_term, taxid))
 		# for fields see: http://www.ncbi.nlm.nih.gov/books/NBK49540/
 		gene_names = ["\"{0}\"".format(e) for e in self.gene_names]
-		if thoroughness == 1: # use gene field and ignore: predicted, genome
+		# use gene field and ignore: predicted, genome
+		if thoroughness == 1:
 			gene_term = ("{0}[GENE]".format(gene_names[0]))
 			for gene_name in gene_names[1:]:
 				gene_term = ("({0}) OR {1}[GENE]".\
 							 format(gene_term, gene_name))
-			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT genome[TI] NOT \
-unverified[TI]".format(taxids_term, gene_term))
-		elif thoroughness == 2: # use title for gene name and ignore: predicted, genome
+			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT \
+genome[TI] NOT unverified[TI]".format(taxids_term, gene_term))
+		# use title for gene name and ignore: predicted, genome
+		elif thoroughness == 2:
 			gene_term = ("{0}[TI]".format(gene_names[0]))
 			for gene_name in gene_names[1:]:
 				gene_term = ("({0}) OR {1}[TI]".\
 							 format(gene_term, gene_name))
-			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT genome[TI] NOT \
-unverified[TI]".format(taxids_term, gene_term))
-		else: # all fields, ignore whole genome shotguns scaffolds and genome assemblies
+			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT \
+genome[TI] NOT unverified[TI]".format(taxids_term, gene_term))
+		# all fields, ignore whole genome shotguns scaffolds and 
+		#  genome assemblies
+		else:
 			gene_term = ("{0}".format(gene_names[0]))
 			for gene_name in gene_names[1:]:
 				gene_term = ("({0}) OR {1}".\
 							 format(gene_term, gene_name))
-			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT shotgun[TI] NOT \
-scaffold[TI] NOT assembly[TI] NOT unverified[TI]".format(taxids_term, gene_term))
+			search_term = ("{0} AND ({1}) NOT predicted[TI] NOT \
+shotgun[TI] NOT scaffold[TI] NOT assembly[TI] NOT unverified[TI]".\
+format(taxids_term, gene_term))
 		#print search_term
 		return search_term
 
 	def _search(self, taxids):
-		"""Search GenBank for matches, increasing thoroughness if fewer
-matches than target nseqs"""
+		"""Search GenBank for matches, increasing thoroughness if 
+fewer matches than target nseqs"""
 		# record seqids of all sequences that match query
 		# move to next thoroughness if too few sequences
 		# if _search is run again, will not return seqids of sequences
@@ -76,14 +82,16 @@ matches than target nseqs"""
 				# keep searching until enough sequences have been
 				#  found or until the max thoroughness has been hit
 				break
-			search_term = self._buildSearchTerm(taxids, self.thoroughness)
+			search_term = self._buildSearchTerm(taxids,\
+				self.thoroughness)
 			seqcount = etools.eSearch(search_term)['Count']
 			if int(seqcount) >= 1:
 				# return ALL matching seqids if more than 0
 				seqids.extend(etools.eSearch(search_term,\
 					retMax = seqcount)['IdList'])
 				# filter those that have already been seen
-				seqids = [e for e in seqids if not e in self.deja_vues]
+				seqids = [e for e in seqids if not e in\
+				self.deja_vues]
 			self.thoroughness += 1
 		self.deja_vues.extend(seqids)
 		self.deja_vues = list(set(self.deja_vues))
@@ -96,12 +104,16 @@ matches than target nseqs"""
 		query = [sequences[randn]]
 		subj = sequences
 		# blast rand seq against all other seqs
-		blast_bool = atools.blast(subj, query, self.minoverlap, self.mingaps)
+		blast_bool = atools.blast(subj, query, self.minoverlap,\
+			self.mingaps)
 		# filtered are all sequences that are true
-		filtered = [sequences[i] for i,e in enumerate(blast_bool) if e]
+		filtered = [sequences[i] for i,e in enumerate(blast_bool) if\
+		e]
 		# sequence pool are all sequences that are false
-		seqpool = [sequences[i] for i,e in enumerate(blast_bool) if not e]
-		# return filtered if there are more than 5 sequences in filtered
+		seqpool = [sequences[i] for i,e in enumerate(blast_bool) if\
+		not e]
+		# return filtered if there are more than 5 sequences in
+		#  filtered
 		if len(filtered) > self.seedsize:
 			return filtered,seqpool
 		# else return empty list of filtered and the sequences
@@ -132,7 +144,9 @@ by searching features."""
 				except ValueError:
 					# catch value errors raised for sequences
 					#  with "fuzzy" positions
-					return ()
+					# TODO: what are fuzzy positions and can I use
+					#  them?
+					return record
 				else:
 					return found_seq
 
@@ -188,9 +202,11 @@ by searching features."""
 				sequences = []
 				downloaded = []
 				lower = 0
-				while len(sequences) < self.nseqs and lower != len(seqids):
+				while len(sequences) < self.nseqs and lower != \
+				len(seqids):
 					upper = min(len(seqids), lower + 1000)
-					downloaded.extend(self._download(seqids[lower:upper]))
+					downloaded.extend(self._download(seqids[lower:\
+						upper]))
 					if not downloaded:
 						break
 					filtered,downloaded = self._filter(downloaded)
@@ -219,13 +235,15 @@ matches in GenBank"""
 			types = None
 		# else find all types for each gene
 		else:
-			search_types = [genedict[e[0]]['type'] for e in searchlist]
-			# if no types or search types and types have same number as
-			#  unique types, return gene with max n
+			search_types = [genedict[e[0]]['type'] for e in \
+			searchlist]
+			# if no types or search types and types have same number
+			#  as unique types, return gene with max n
 			if len(types) == 0 or len(list(set(search_types))) ==\
 			len(list(set(types))):
 				gene_i = ns.index(max(ns))
-			# else, loop until the next type with the highest n is reached
+			# else, loop until the next type with the highest n is
+			#  reached
 			else:
 				while True:
 					gene_i = ns.index(max(ns))
@@ -238,7 +256,8 @@ matches in GenBank"""
 	alltipids = [namesdict[e]["txids"] for e in namesdict.keys()]
 	outgroupids = namesdict['outgroup']['txids']
 	searchlist = []
-	outgroup_bool = [] # list of bools for number of genes w/o outgroup seqs
+	# list of bools for number of genes w/o outgroup seqs
+	outgroup_bool = []
 	for gene in genedict.keys():
 		logging.info(" .... checking [{0}]".format(gene))
 		# first check if its suitable for this taxonomic group
@@ -254,9 +273,11 @@ matches in GenBank"""
 				seedsize = 0, maxtrys = 0, mingaps = 0,\
 				minoverlap = 0, maxlen = 0, minlen = 0)
 				res = downloader._search(tipids)
-				# if gene is deep or both, then make sure it has outgroup
+				# if gene is deep or both, then make sure it has
+				#  outgroup
 				if gene_type != 'shallow':
-					# if outgroupids do not have sequences, move to next genes
+					# if outgroupids do not have sequences, move to
+					#  next genes
 					if tipids == outgroupids:
 						if len(res) < minnseq:
 							outgroup_bool.append(False)
