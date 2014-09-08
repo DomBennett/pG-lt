795aae9cc0cbcd823d904264038800450a4d22c6
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-09-08 18:39:10 +0100

    Adapted for running alignment stage in session

--- misc/setup_logging.py
+++ misc/setup_logging.py
@@ -4,3 +4,6 @@ logger.setLevel(logging.INFO)
 console = logging.StreamHandler()
 console.setFormatter(logging.Formatter('%(message)s'))
 logger.addHandler(console)
+
+import mpe
+mpe.stages.alignment_stage.run()
