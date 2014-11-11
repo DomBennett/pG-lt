56fbb7793f3505cad955c2d494ee2b578a660c61
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-11-11 22:10:25 +0000

    Minor edit

--- misc/setup_runner.py
+++ misc/setup_runner.py
@@ -7,4 +7,4 @@ import os
 Runner = pglt.tools.system_tools.Runner
 folders = ['SE1_2007__Parra_1']
 runner = Runner(folders, 1, os.getcwd())
-runner.run(folders, [1])
+runner.run(folders, ['1', '2'])
