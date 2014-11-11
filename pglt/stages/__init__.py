1a937a7b77cb3724d235fc0187adee23156d0eed
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-11-11 22:10:42 +0000

    Aesthetic change

--- pglt/stages/__init__.py
+++ pglt/stages/__init__.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
-## D.J. Bennett
-## 26/04/2014
+# D.J. Bennett
+# 26/04/2014
 """
 pglt stages
 """
@@ -10,7 +10,7 @@ import names_stage
 import download_stage
 import alignment_stage
 import phylogeny_stage
-STAGES = {'1': (names_stage.run, '1_names'),\
-'2': (download_stage.run, '2_download'),\
-'3' : (alignment_stage.run, '3_alignment'),\
-'4' : (phylogeny_stage.run, '4_phylogeny')}
+STAGES = {'1': (names_stage.run, '1_names'),
+          '2': (download_stage.run, '2_download'),
+          '3': (alignment_stage.run, '3_alignment'),
+          '4': (phylogeny_stage.run, '4_phylogeny')}
