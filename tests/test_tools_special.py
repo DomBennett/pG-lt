42f178b5ccc6eb35397aa1b4c30b12f4e9d15537
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-11-13 18:05:24 +0000

    Created tests for special_tools

--- tests/test_tools_special.py
+++ tests/test_tools_special.py
@@ -4,3 +4,60 @@
 """
 Tests for special tools.
 """
+
+# PACKAGES
+import os
+import unittest
+import shutil
+import time
+import pickle
+import pglt.tools.special_tools as stools
+
+
+# FUNCTIONS
+def sleep(seconds):
+    time.sleep(seconds)
+
+
+class SpecialTestSuite(unittest.TestCase):
+
+    def test_timeit(self):
+        # time time.sleep for 0.1 second
+        res, s = stools.timeit(sleep, seconds=0.1)
+        self.assertAlmostEqual(s, 0.1, places=2)
+
+    def test_get_threads_local_false(self):
+        res = stools.getThreads()
+        self.assertTrue(isinstance(res, int))
+        self.assertTrue(res > 0)
+
+    def test_get_threads_local_true(self):
+        # create .threads.p
+        with open(".threads.p", "wb") as file:
+            pickle.dump(2, file)
+        res = stools.getThreads(True)
+        self.assertEqual(res, 2)
+        os.remove(".threads.p")
+
+    def test_clean(self):
+        # create dummy folders and files
+        threads_file = os.path.join('afolder', '.threads.p')
+        names_folder = os.path.join('afolder', '1_names')
+        os.mkdir('afolder')
+        os.mkdir(names_folder)
+        with open(threads_file, "wb") as file:
+            pickle.dump(2, file)
+        # clean
+        stools.clean()
+        # make sure they don't exist
+        self.assertFalse(os.path.isfile(threads_file))
+        self.assertFalse(os.path.isdir(names_folder))
+
+    def tearDown(self):
+        if os.path.isfile('.threads.p'):
+            os.remove(".threads.p")
+        if os.path.isdir('afolder'):
+            shutil.rmtree('afolder')
+
+if __name__ == '__main__':
+    unittest.main()
