be1b6d86951246afed08ad5b846895d8dd90538c
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-11-16 20:55:33 +0000

    Finished setup tools tests

--- tests/test_tools_setup.py
+++ tests/test_tools_setup.py
@@ -38,12 +38,21 @@ class SetupTestSuite(unittest.TestCase):
 
     def tearDown(self):
         stools.getThreads = self.true_getThreads
-        if os.path.isdir('folder_1'):
-            shutil.rmtree('folder_1')
-        if os.path.isdir('folder_2'):
-            shutil.rmtree('folder_2')
-        if os.path.isfile('log.txt'):
-            os.remove('log.txt')
+        setup_files = ['.paradict.p', '.namesdict.p', '.genedict.p', 'log.txt',
+                       'info.txt', '.terms.p', 'names.txt']
+        while setup_files:
+            try:
+                setup_file = setup_files.pop()
+                os.remove(setup_file)
+            except OSError:
+                pass
+        setup_folders = ['folder_1', 'folder_2']
+        while setup_folders:
+            try:
+                setup_folder = setup_folders.pop()
+                shutil.rmtree(setup_folder)
+            except OSError:
+                pass
 
     def test_calc_workers(self):
         # for 100 folder:
@@ -134,26 +143,70 @@ class SetupTestSuite(unittest.TestCase):
         self.assertIsNone(res)
 
     def test_prime(self):
-        pass
-
-    def test_sort_args(self):
-        pass
+        arguments = {'genedict': {}, 'paradict': {}, 'terms': []}
+        directory = '.'
+        stools.prime(directory, arguments)
+        self.assertTrue(os.path.isfile('.genedict.p'))
+        self.assertTrue(os.path.isfile('.paradict.p'))
+        self.assertTrue(os.path.isfile('.terms.p'))
+        os.remove('.genedict.p')
+        os.remove('.paradict.p')
+        os.remove('.terms.p')
 
     def test_record_pars(self):
-        pass
+        paradict = {'a_parameter': 'a_value'}
+        self.assertTrue(isinstance(stools.recordPars(paradict), str))
 
     def test_record_gpars(self):
-        pass
+        genedict = {'a_gene': {'a_parameter': 'a_value'}}
+        self.assertTrue(isinstance(stools.recordGpars(genedict), str))
 
     def test_read_in_names(self):
-        pass
+        # write a list of names and read in
+        with open('names.txt', 'w') as file:
+            file.write('a_name\n')
+        names = stools.readInNames('.')
+        self.assertEqual(names[0], 'a_name')
+        os.remove('names.txt')
 
     def test_read_in_gene_pars(self):
-        pass
+        # read in test parameters in /data
+        default_file = os.path.join('data', 'test_default_gene_parameters.csv')
+        test_file = os.path.join('data', 'test_gene_parameters.csv')
+        res = stools.readInGenePars(test_file, default_file)
+        # COI minlen should be 'a different value'
+        self.assertEqual(res['COI']['minlen'], 'a different value')
 
     def test_read_in_pars(self):
-        pass
+        # read in test parameters in /data
+        default_file = os.path.join('data', 'test_default_parameters.csv')
+        test_file = os.path.join('data', 'test_parameters.csv')
+        res = stools.readInPars(test_file, default_file)
+        # nseqs should be 'a different value'
+        self.assertEqual(res['nseqs'], 'a different value')
 
+    def test_sort_args(self):
+        # make sure error is raised if no names.txt
+        directory = '.'
+        email = 'an.email'
+        logger = stools.setUpLogging(verbose=False, debug=False,
+                                     logname='testlogger')
+        default_pars_file = os.path.join('data', 'test_default_parameters.csv')
+        default_gpars_file = os.path.join('data',
+                                          'test_default_gene_parameters.csv')
+        with self.assertRaises(stools.PrimingError):
+            stools.sortArgs(directory, email, logger, default_pars_file,
+                            default_gpars_file)
+        # create a names.txt and test arguments returned
+        names = ['name1', 'name2', 'name3', 'name4', 'name5', 'name6']
+        with open('names.txt', 'w') as file:
+            for name in names:
+                file.write(name + '\n')
+        res = stools.sortArgs(directory, email, logger, default_pars_file,
+                              default_gpars_file)
+        self.assertTrue(isinstance(res['terms'], list))
+        self.assertTrue(isinstance(res['genedict'], dict))
+        self.assertTrue(isinstance(res['paradict'], dict))
 
 if __name__ == '__main__':
     unittest.main()
