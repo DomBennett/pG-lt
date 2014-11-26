2e0ff4027dad7f7c1b2e742a2f5c65b851933945
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-11-26 14:14:53 +0000

    Minor change
    Feed arguments to qsub

--- misc/example_script.sh
+++ misc/example_script.sh
@@ -3,10 +3,11 @@
 #PBS -l walltime=00:01:00
 
 # ensure relevant modules are loaded e.g. mpi, intel-suite ...
-# to run: qsub 'script_name'.sh 'abs_path_to_parent_folder'
+# to run: qsub -v folder='abs_path_to_parent_folder' 'script_name'.sh
 
 # find pglt_farm.py in $PATH
-FARM=`which pglt_farm.py`
-# run FARM with an absolute path to a parent folder
-echo 'Running with ' $1
-mpiexec python $FARM $1
+FARMSCRIPT=`which pglt_farm.py`
+# run $FARMSCRIPT with an absolute path to a parent folder
+FOLDER=${folder}
+echo 'Running with' $FOLDER
+mpiexec python $FARMSCRIPT $FOLDER
