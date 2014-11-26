334001e4d8ef4b57f2bd9c13090f94b413d2ba27
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-11-26 12:52:58 +0000

    Minor edit

--- misc/example_script.sh
+++ misc/example_script.sh
@@ -5,4 +5,7 @@
 # to run: qsub 'script_name'.sh
 # ensure relevant modules are loaded e.g. mpi, intel-suite ...
 
-mpiexec python pglt_farm.py abs_path_to_folder
+# find pglt_farm.py in $PATH
+FARM = `which pglt_farm.py`
+# run FARM with a directory
+mpiexec python $FARM abs_path_to_folder
