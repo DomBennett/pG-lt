53b3f7ff11c600e6467bf8320b7e73d90eea9fdd
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-11-26 13:20:14 +0000

    Minor correction

--- misc/example_script.sh
+++ misc/example_script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=2
+#PBS -l select=1:ncpus=12
 #PBS -l walltime=00:01:00
 
-# to run: qsub 'script_name'.sh
 # ensure relevant modules are loaded e.g. mpi, intel-suite ...
+# to run: qsub 'script_name'.sh 'abs_path_to_parent_folder'
 
 # find pglt_farm.py in $PATH
-FARM = `which pglt_farm.py`
-# run FARM with a directory
-mpiexec python $FARM abs_path_to_folder
+FARM=`which pglt_farm.py`
+# run FARM with an absolute path to a parent folder
+echo 'Running with ' $1
+mpiexec python $FARM $1
