31bdeaee7fca5b8d9c8d49289d37d05f7a20f016
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-04-29 18:57:22 +0100

    Set dependencies with new script

---
+++ pglt_set_dependencies.py
@@ -0,0 +1,141 @@
+#! /usr/bin/env python
+# D.J. Bennett
+# 29/04/2015
+"""
+Set dependencies for pG-lt
+"""
+import os
+import subprocess
+import re
+import argparse
+import pickle
+from tabulate import tabulate
+from pglt import _RAXML as raxml
+from pglt import _MAFFT as mafft
+from pglt import _MAFFTQ as mafftq
+from pglt import _MAFFTX as mafftx
+from pglt import _BLASTN as blastn
+from pglt import _ROOT as root
+print root
+
+# GLOBALS
+abserr_msg = 'Absolute paths to dependencies only. [{0}] is not an absolute \
+path.'
+with open(os.path.join(root, 'dependencies.p'), "rb") as file:
+    depsdict = pickle.load(file)
+
+
+# FUNCTIONS
+def createParser():
+    """Create parser for command-line"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-mafft", help="path to `mafft`, by default will search \
+current working directory", type=str)
+    parser.add_argument("-mafftq", help="path to `mafftq`, by default will \
+search current working directory", type=str)
+    parser.add_argument("-mafftx", help="path to `mafftx`, by default will \
+search current working directory", type=str)
+    parser.add_argument("-raxml", help="path to `raxml`, by default will search \
+current working directory", type=str)
+    parser.add_argument("-blastn", help="path to `blastn`, by default will \
+search current working directory", type=str)
+    parser.add_argument('--overwrite', help='overwrite dependencies already \
+added to pG-lt', action='store_true')
+    return parser
+
+
+def runCommand(args):
+    """Run and return command"""
+    # run, read and kill
+    process = subprocess.Popen(args, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    info = process.stdout.read()
+    process.kill()
+    return info.strip()
+
+
+def getVersion(args):
+    """Return version number of program"""
+    # first check its absolute
+    if not os.path.isabs(args[0]):
+        print abserr_msg.format(args[0])
+        return
+    # check if program exists
+    try:
+        info = runCommand(args)
+        # find version number, extract and strip of digits
+        pattern = '(v|version)?\s?[0-9]\.[0-9]+'
+        res = re.search(pattern, info)
+        version = info[res.span()[0]:res.span()[1]]
+        non_decimal = re.compile(r'[^\d.]+')
+        version = non_decimal.sub('', version)
+        return float(version)
+    except OSError:
+        return False
+
+
+if __name__ == '__main__':
+    args = createParser().parse_args()
+    print '\nSearching and setting dependencies to pG-lt ....'
+    changes = False
+    # MAFFT
+    if args.mafft and not mafft or args.overwrite:
+        version = getVersion([args.mafft, '-u'])
+        if version and version > 7.0:
+            depsdict['mafft'] = args.mafft
+            changes = True
+        else:
+            print 'No MAFFT detected -- requires MAFFT v7+'
+    # MAFFTQ
+    if args.mafftq and not mafftq or args.overwrite:
+        version = getVersion([args.mafftq, '-u'])
+        if version and version > 7.0:
+            depsdict['mafftq'] = args.mafftq
+            changes = True
+        else:
+            print 'No mafft-qinsi detected'
+    # MAFFTX
+    if args.mafftx and not mafftx or args.overwrite:
+        version = getVersion([args.mafftx, '-u'])
+        if version and version > 7.0:
+            depsdict['mafftx'] = args.mafftx
+            changes = True
+        else:
+            print 'No mafft-xinsi detected'
+    # RAXML
+    if args.raxml and not raxml or args.overwrite:
+        version = getVersion([args.raxml, '-version'])
+        if version and version > 7.0:
+            depsdict['raxml'] = args.raxml
+            changes = True
+        else:
+            print 'No RAxML detected -- requires RAxML v7+'
+    # BLAST
+    if args.blastn and not blastn or args.overwrite:
+        version = getVersion([args.blastn, '-h'])
+        if version and version > 2.0:
+            depsdict['blastn'] = args.blastn
+            changes = True
+        else:
+            print 'No Stand-alone BLAST detected -- requires BLAST suite v2+'
+    if changes:
+        with open(os.path.join(root, 'dependencies.p'), "wb") as file:
+            pickle.dump(depsdict, file)
+        print 'Done.\n'
+    else:
+        print 'No dependencies given.\n'
+    # CHECK
+    # create table
+    print('-'*70)
+    print(' ' * 23 + 'Set dependencies' + ' ' * 23)
+    print('-'*70)
+    deps = [('mafft', '3'), ('mafftq', '-'), ('mafftx', '-'),
+            ('raxml', '4'), ('blastn', '2-3')]
+    rows = []
+    headers = ['Dependency', 'Status', 'Stages required', 'Path']
+    for d, e in deps:
+        if depsdict[d]:
+            rows.append([d, 'Present', e, depsdict[d]])
+        else:
+            rows.append([d, 'Absent', e, '-'])
+    print(tabulate(rows, headers, tablefmt="simple") + '\n')
