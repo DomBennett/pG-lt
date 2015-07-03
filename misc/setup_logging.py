4d026a3dfed0d1968c58c911648d10bebc52059a
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-07-03 15:36:21 +0100

    Added comments

--- misc/setup_logging.py
+++ misc/setup_logging.py
@@ -1,9 +1,18 @@
-import logging
-logger = logging.getLogger('')
-logger.setLevel(logging.DEBUG)
-console = logging.StreamHandler()
-console.setFormatter(logging.Formatter('%(message)s'))
-logger.addHandler(console)
+#! /usr/bin/env python
+"""
+Run me in a pG-lt setup folder to run individual stages in a python session
+"""
 
+# LIBS
+import logging
 import pglt
+
+# SETUP LOGGER
+logger = logging.getLogger('')  # this creates a root logger
+logger.setLevel(logging.DEBUG)  # print debug messages and above
+console = logging.StreamHandler()  # create console stream
+console.setFormatter(logging.Formatter('%(message)s'))  # simple message
+logger.addHandler(console)  # add to logger
+
+# RUN (names, download, alignment of phylogeny)
 pglt.stages.alignment_stage.run()
